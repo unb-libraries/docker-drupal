5171c962996b543ec7c1bade49e35a35667f9e36
Jacob Sanford <jsanford@unb.ca>
2015-08-26 20:20:19 -0300

    Whitespace cleanup

--- init/55_wait_for_mysql_server.sh
+++ init/55_wait_for_mysql_server.sh
@@ -10,8 +10,8 @@ nc -zw10 ${MYSQL_PORT_3306_TCP_ADDR} ${MYSQL_PORT_3306_TCP_PORT}
 RETVAL=$?
 while [ $RETVAL -ne 0 ]
 do
-   nc -zw10 ${MYSQL_PORT_3306_TCP_ADDR} ${MYSQL_PORT_3306_TCP_PORT}
-   RETVAL=$?
-   echo -e "\t Waiting for MySQL server on $MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT..."
-   sleep 10
+  nc -zw10 ${MYSQL_PORT_3306_TCP_ADDR} ${MYSQL_PORT_3306_TCP_PORT}
+  RETVAL=$?
+  echo -e "\t Waiting for MySQL server on $MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT..."
+  sleep 10
 done
