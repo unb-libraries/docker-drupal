6ba8634b26219cc73bb338323d21ba4eb2058b4e
Jacob Sanford <jsanford@unb.ca>
2015-09-02 14:33:39 -0300

    Fix overrides applications

--- init/61_apply_settings_overrides.sh
+++ init/61_apply_settings_overrides.sh
@@ -3,9 +3,10 @@
 # Apply settings overrides
 OVERRIDE_SOURCE_FILE="${TMP_DRUPAL_BUILD_DIR}/settings_override.php"
 OVERRIDE_TARGET_FILE="${DRUPAL_ROOT}/sites/default/settings.php"
+
 if [ -e $OVERRIDE_SOURCE_FILE ]; then
 while read -u 10 CONF_LINE; do
-  TRIMMED_LINE=`echo "$CONF_LINE" | xargs`
-  grep -q "^${TRIMMED_LINE}$" $OVERRIDE_TARGET_FILE || echo "$TRIMMED_LINE" >> $OVERRIDE_TARGET_FILE
+  TRIMMED_LINE=`echo "$CONF_LINE" | xargs -0`
+  grep -q "^$TRIMMED_LINE\$" $OVERRIDE_TARGET_FILE || echo "$TRIMMED_LINE" >> $OVERRIDE_TARGET_FILE
 done 10<$OVERRIDE_SOURCE_FILE
 fi
