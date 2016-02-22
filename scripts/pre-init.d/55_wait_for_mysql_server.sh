c2223c95b110c7736448805e7d7e1b2ad8d3301a
Jacob Sanford <jsanford@unb.ca>
2016-02-22 13:33:00 -0400

    Check for MySQL server environment vars

--- scripts/pre-init.d/55_wait_for_mysql_server.sh
+++ scripts/pre-init.d/55_wait_for_mysql_server.sh
@@ -5,6 +5,16 @@
 MYSQL_PORT_3306_TCP_ADDR="${MYSQL_PORT_3306_TCP_ADDR:-$(echo $MYSQL_HOSTNAME)}"
 MYSQL_PORT_3306_TCP_PORT="${MYSQL_PORT_3306_TCP_PORT:-$(echo $MYSQL_PORT)}"
 
+# Check if MySQL env vars exist.
+if ["${MYSQL_PORT_3306_TCP_ADDR}" == ""]; then
+ echo 'A MySQL server IP address has not been set in $MYSQL_HOSTNAME'
+ exit 1
+fi
+if ["${MYSQL_PORT_3306_TCP_PORT}" == ""]; then
+ echo 'A MySQL server port has not been set in $MYSQL_PORT'
+ exit 1
+fi
+
 # Check to see if MySQL is accepting connections
 nc -zw10 ${MYSQL_PORT_3306_TCP_ADDR} ${MYSQL_PORT_3306_TCP_PORT}
 RETVAL=$?
