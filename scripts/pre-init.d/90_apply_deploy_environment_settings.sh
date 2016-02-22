d2a11ce051db8fd4ec8beee659509781733eb17d
Jacob Sanford <jsanford@unb.ca>
2016-02-22 12:56:34 -0400

    Copy devel module back if it exists to avoid errors on re-launch

--- scripts/pre-init.d/90_apply_deploy_environment_settings.sh
+++ scripts/pre-init.d/90_apply_deploy_environment_settings.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 if [ "$DEPLOY_ENV" = "dev" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes en devel field_ui views_ui
+  rsync -a --progress --no-perms --no-owner --no-group ${DRUPAL_ROOT}/modules/devel ${DRUSH_MAKE_TMPROOT}/modules
 elif [ "$DEPLOY_ENV" = "prod" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes pm-uninstall devel field_ui views_ui dblog
 fi
