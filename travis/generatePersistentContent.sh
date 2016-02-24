fac1a73700eda00e0199407e8ee8393ebeda0d42
Jacob Sanford <jsanford@unb.ca>
2016-02-24 09:43:03 -0400

    Enable devel_generate when testing in travis

--- travis/generatePersistentContent.sh
+++ travis/generatePersistentContent.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default en --yes devel
+docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default en --yes devel_generate
 docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default --yes genc 5 --types=article
