6acf53ac23671ded1f3ab97347c226c5e00f9b43
Jacob Sanford <jsanford@unb.ca>
2016-02-24 09:58:44 -0400

    Remove simpletest after testing

--- travis/testDrupal.sh
+++ travis/testDrupal.sh
@@ -4,3 +4,4 @@ docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scr
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\node\Tests\PageViewTest"'
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\system\Tests\File\UrlRewritingTest"'
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\file\Tests\FilePrivateTest"'
+docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default pm-uninstall --yes simpletest
