091532df7224123c228e30d6f1ed9d89027d3abe
Jacob Sanford <jsanford@unb.ca>
2016-02-24 09:43:39 -0400

    Run scripts in bash for travis

--- travis/testPersistentContent.sh
+++ travis/testPersistentContent.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 curl -I --fail http://127.0.0.1/node/5
--- travis/triggerDockerHubBuild.sh
+++ travis/triggerDockerHubBuild.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 curl -H "Content-Type: application/json" --data '{"docker_tag": "alpine-nginx-8.x"}' -X POST https://registry.hub.docker.com/u/unblibraries/drupal/trigger/${DOCKER_HUB_TRIGGER_TOKEN}/
