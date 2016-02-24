52fb889eaac4c023f768bbb4680ebc6239765a38
Jacob Sanford <jsanford@unb.ca>
2016-02-24 10:13:59 -0400

    Disable devel_generate after generating content in travis tests

--- travis/generatePersistentContent.sh
+++ travis/generatePersistentContent.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default en --yes devel_generate
 docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default --yes genc 5 --types=article
+docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default pm-uninstall --yes devel_generate
