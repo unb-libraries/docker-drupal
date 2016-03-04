243f994c09cb04c8dfbcdacc01500748887cd33e
Jacob Sanford <jsanford@unb.ca>
2016-03-04 09:40:39 -0400

    Add openssh-client if cloning existing site.

--- scripts/pre-init.d/65_clone_remote_instance.sh
+++ scripts/pre-init.d/65_clone_remote_instance.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 # Transfer data from a remote site to the local instance.
+# Convert your private key to an ENV compatible string with "cat private_key | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g'"
 
 # Ensure that all details are provided and this is the a first time deployment. Otherwise, skip this.
 if [[ -n "$DRUSH_TRANSFER_KEY" && -n "$DRUSH_TRANSFER_USER" && -n "$DRUSH_TRANSFER_HOST" && -n "$DRUSH_TRANSFER_PATH" && -n "$DRUSH_TRANSFER_URI" && ! -f /tmp/DRUPAL_DB_LIVE && ! -f /tmp/DRUPAL_FILES_LIVE ]] ; then
-
+  echo "Cloning Remote Instance.."
   # Write remote auth key to a temporary location and set appropriate permissions.
   cat <<EOT >> /tmp/remote_auth_key
 $DRUSH_TRANSFER_KEY
@@ -23,12 +24,18 @@ EOT
 );
 EOT
 
+  # Install openssh-client
+  apk --update add openssh-client
+
   # If we can access the site and drush reports a normal status, then transfer the data.
   if [[ $(drush @live status --alias-path=/tmp/drush-aliases) =~ "Successful" ]]; then
     DRUSH_BIN='drush --yes --verbose --alias-path=/tmp/drush-aliases --uri=default'
     cd ${DRUPAL_ROOT}
     $DRUSH_BIN @live status
     $DRUSH_BIN rsync @live:%files @self:%files --omit-dir-times --no-p --no-o --exclude-paths="css:js:styles:imagecache:ctools:tmp"
+    $DRUSH_BIN rsync @live:%modules @self:%modules --omit-dir-times --no-p --no-o
+    $DRUSH_BIN rsync @live:%themes @self:%themes --omit-dir-times --no-p --no-o
     $DRUSH_BIN sql-sync @live @self
+    $DRUSH_BIN cc all
   fi
 fi
