83378f35b37b176701fa605f2f4f1b11cbd2e1e1
Jacob Sanford <jsanford@unb.ca>
2016-05-20 15:48:20 -0300

    Install drupal console if DEPLOY_ENV=dev

--- scripts/pre-init.d/90_apply_deploy_environment_settings.sh
+++ scripts/pre-init.d/90_apply_deploy_environment_settings.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 if [ "$DEPLOY_ENV" = "dev" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes en devel field_ui views_ui
+  curl https://drupalconsole.com/installer -L -o /usr/local/bin/drupal && chmod +x /usr/local/bin/drupal && /usr/local/bin/drupal init && /usr/local/bin/drupal check
   rsync -a --progress --no-perms --no-owner --no-group ${DRUPAL_ROOT}/modules/contrib/devel ${DRUSH_MAKE_TMPROOT}/modules/contrib
 elif [ "$DEPLOY_ENV" = "prod" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes pm-uninstall devel field_ui views_ui dblog
