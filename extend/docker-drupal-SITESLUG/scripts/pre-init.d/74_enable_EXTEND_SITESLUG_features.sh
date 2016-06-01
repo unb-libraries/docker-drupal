a6544d751c5e1fb41907155401a2eb397e839914
Jacob Sanford <jsanford@unb.ca>
2016-06-01 11:48:29 -0300

    Remove extend in favour of stand-alone repo

--- extend/docker-drupal-SITESLUG/scripts/pre-init.d/74_enable_EXTEND_SITESLUG_features.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-# Local alterations for your instance.
-# i.e. drush --root=${DRUPAL_ROOT} --uri=default --yes en thirty_two_project
--- extend/docker-drupal-SITESLUG/scripts/pre-init.d/99_add_local_user_group_write.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-chgrp -R "${LOCAL_USER_GROUP}" "${DRUPAL_ROOT}"
-chmod g+w -R "${DRUPAL_ROOT}"
