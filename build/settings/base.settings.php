f6fd5bf42aaec98dd095e4737eedb0640945a9ea
Jacob Sanford <jsanford@unb.ca>
2016-06-02 13:27:49 -0300

    Add defaults for environment includes from extension

--- build/settings/base.settings.php
+++ build/settings/base.settings.php
@@ -3,3 +3,14 @@
 Include all settings overrides here
 # require_once 'settings_file.inc';
 */
+
+// Load environment based includes.
+if (isset($_SERVER['APPLICATION_ENV'])) {
+  $environment = strtolower($_SERVER['APPLICATION_ENV']);
+  $environment_include = dirname(__FILE__) . "/settings.$environment.inc";
+  if (file_exists($environment_include)) {
+    include_once $environment_include;
+  }
+}
+
+// Add common includes below.
