7e3481181a1e5e69c082384e2f33ed2e52968436
Jacob Sanford <jsanford@unb.ca>
2016-06-05 18:39:38 -0300

    Update travis tests to generalized compose V2 versions.

--- travis/testDrupal.sh
+++ travis/testDrupal.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Runs Drupal tests.
 docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default en --yes simpletest
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\user\Tests\UserLoginTest"'
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\node\Tests\PageViewTest"'
