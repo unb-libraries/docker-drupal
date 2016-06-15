01bd22e7672899c2ea736bc000a323bfcf6a5159
Jacob Sanford <jsanford@unb.ca>
2016-06-15 12:02:01 -0300

    Symlink behat tests instead of copying them

--- scripts/pre-init.d/94_install_testing_tools.sh
+++ scripts/pre-init.d/94_install_testing_tools.sh
@@ -2,7 +2,7 @@
 if [ "$DRUPAL_TESTING_TOOLS" != "FALSE" ]; then
   cd ${DRUPAL_ROOT}
   /usr/local/bin/composer require drupal/drupal-extension='~3.0' --prefer-dist
-  rsync -a --progress --no-perms --no-owner --no-group ${TMP_DRUPAL_BUILD_DIR}/behat.yml ${DRUPAL_ROOT}
-  rsync -a --progress --no-perms --no-owner --no-group ${TMP_DRUPAL_BUILD_DIR}/features ${DRUPAL_ROOT}
+  ln -s ${TMP_DRUPAL_BUILD_DIR}/behat.yml ${DRUPAL_ROOT}/
+  ln -s ${TMP_DRUPAL_BUILD_DIR}/features ${DRUPAL_ROOT}/features
   ./vendor/bin/behat --init
 fi
