0a041a0d5302059455506737a203c2083661f3ee
Jacob Sanford <jsanford@unb.ca>
2016-06-17 08:07:21 -0300

    Use DRUPAL_CONFIGURATION_DIR in import script

--- scripts/configImport.sh
+++ scripts/configImport.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-drush --root=${DRUPAL_ROOT} --uri=default --yes config-import --source=/app/configuration
+drush --root=${DRUPAL_ROOT} --uri=default --yes config-import --source=${DRUPAL_CONFIGURATION_DIR}
