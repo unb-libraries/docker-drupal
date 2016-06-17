d72cdbc11d93b8344b3aedbc0582bdfa02472866
Jacob Sanford <jsanford@unb.ca>
2016-06-17 15:38:55 -0300

    Be specific with PHP version for Drush

--- scripts/drupalCron.sh
+++ scripts/drupalCron.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-su nginx -s /bin/sh -c 'drush --root=${DRUPAL_ROOT} --uri=default --yes core-cron'
+su nginx -s /bin/sh -c 'DRUSH_PHP=/usr/bin/php drush --root=${DRUPAL_ROOT} --uri=default --yes core-cron'
