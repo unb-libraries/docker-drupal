4e2aa6beed1d1936429a672591cd67ddc3e5e218
Jacob Sanford <jsanford@unb.ca>
2016-09-30 15:40:37 -0300

    Only install drupal console locally

--- scripts/pre-init.d/90_environment_settings.sh
+++ scripts/pre-init.d/90_environment_settings.sh
@@ -2,7 +2,15 @@
 if [ "$DEPLOY_ENV" = "local" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes --destination=modules/contrib dl devel
   drush --root=${DRUPAL_ROOT} --uri=default --yes en devel field_ui views_ui
-  curl https://drupalconsole.com/installer -L -o /usr/local/bin/drupal && chmod +x /usr/local/bin/drupal && /usr/local/bin/drupal init && /usr/local/bin/drupal check
+
+  # Install Drupal Console
+  cd ${DRUPAL_ROOT}
+  composer require drupal/console:~1.0 \
+  --prefer-dist \
+  --optimize-autoloader \
+  --sort-packages
+  ln -s /app/html/vendor/bin/drupal /usr/local/bin/drupal
+
   mkdir -p ${DRUSH_MAKE_TMPROOT}/modules/contrib
   rsync -a --progress --no-perms --no-owner --no-group ${DRUPAL_ROOT}/modules/contrib/devel ${DRUSH_MAKE_TMPROOT}/modules/contrib
 elif [ "$DEPLOY_ENV" = "prod" ]; then
