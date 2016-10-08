9fc8175d9897005e77bfad8e9b59942f0e59d2c4
Jacob Sanford <jsanford@unb.ca>
2016-10-08 13:27:33 -0300

    Update travis tests to override drush policy

--- travis/generatePersistentContent.sh
+++ travis/generatePersistentContent.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 # Generate content to test persistence in a restart.
-docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default en --yes devel_generate
+docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default en --yes --pm-force devel_generate
 docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default --yes genc 5 --types=article
-docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default pm-uninstall --yes devel_generate
+docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default pm-uninstall --yes --pm-force devel_generate
--- travis/testDrupal.sh
+++ travis/testDrupal.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # Runs Drupal tests.
-docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default en --yes simpletest
+docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default en --yes --pm-force simpletest
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\user\Tests\UserLoginTest"'
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\node\Tests\PageViewTest"'
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\system\Tests\File\UrlRewritingTest"'
 docker exec dockerdrupal_drupal_1 su nginx -s /bin/sh -c 'php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class "\Drupal\file\Tests\FilePrivateTest"'
-docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default pm-uninstall --yes simpletest
+docker exec dockerdrupal_drupal_1 drush --root=/app/html --uri=default pm-uninstall --yes --pm-force simpletest
