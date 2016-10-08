ecaceff16c40d50b53642078da4a9d6efb6b882a
Jacob Sanford <jsanford@unb.ca>
2016-10-08 13:06:01 -0300

    Use proper comparison operator for environment

--- scripts/pre-init.d/90_environment_settings.sh
+++ scripts/pre-init.d/90_environment_settings.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-if [ "$DEPLOY_ENV" = "local" ]; then
+if [ "$DEPLOY_ENV" == "local" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes en devel field_ui views_ui
   mkdir -p ${DRUSH_MAKE_TMPROOT}/modules/contrib
-elif [ "$DEPLOY_ENV" = "prod" ]; then
+elif [ "$DEPLOY_ENV" == "prod" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes pm-uninstall devel field_ui views_ui dblog
 fi
