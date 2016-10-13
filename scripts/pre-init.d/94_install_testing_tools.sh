e7f303ef02661daa70e769b1a35f7e54a9ecb6a8
Jacob Sanford <jsanford@unb.ca>
2016-10-13 12:14:05 -0300

    Fix permissions issue for local deploys

--- scripts/pre-init.d/94_install_testing_tools.sh
+++ scripts/pre-init.d/94_install_testing_tools.sh
@@ -4,4 +4,5 @@ if [ "$DRUPAL_TESTING_TOOLS" != "FALSE" ]; then
   ln -s ${TMP_DRUPAL_BUILD_DIR}/behat.yml ${DRUPAL_ROOT}/
   ln -s ${TMP_DRUPAL_BUILD_DIR}/features ${DRUPAL_ROOT}/features
   ./vendor/bin/behat --init
+  chmod -R g+w features/
 fi
