21f925fec9ddd6b1b6993448b37f3b3aaaa8cfb7
Jacob Sanford <jsanford@unb.ca>
2016-10-26 10:15:06 -0300

    Enable dev mode on console when starting in local env

--- scripts/pre-init.d/90_environment_settings.sh
+++ scripts/pre-init.d/90_environment_settings.sh
@@ -2,6 +2,9 @@
 if [ "$DEPLOY_ENV" == "local" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes en devel field_ui views_ui
   mkdir -p ${DRUPAL_BUILD_TMPROOT}/modules/contrib
+  cd ${DRUPAL_ROOT}
+  drupal init -n
+  drupal site:mode dev
 elif [ "$DEPLOY_ENV" == "prod" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes pm-uninstall devel field_ui views_ui dblog
 fi
