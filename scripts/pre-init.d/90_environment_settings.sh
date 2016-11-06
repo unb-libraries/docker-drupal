e46cdda3d2970d13dfc480e45ccef21401c6e3bb
Jacob Sanford <jsanford@unb.ca>
2016-11-06 09:55:34 -0400

    Alter twig config to disable caching, enable debug

--- scripts/pre-init.d/90_environment_settings.sh
+++ scripts/pre-init.d/90_environment_settings.sh
@@ -5,6 +5,7 @@ if [ "$DEPLOY_ENV" == "local" ]; then
   cd ${DRUPAL_ROOT}
   drupal init -n
   drupal site:mode dev
+  sed -i "s|twig.config:.*|twig.config: { debug: true, auto_reload: null, cache: false }|g" ${DRUPAL_ROOT}/sites/default/services.yml
 elif [ "$DEPLOY_ENV" == "prod" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes pm-uninstall devel field_ui views_ui dblog
 fi
