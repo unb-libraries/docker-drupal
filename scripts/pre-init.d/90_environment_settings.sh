104f7ec2f5b6dfa6da3df2da96bb97b80e7886e7
Jacob Sanford <jsanford@unb.ca>
2017-01-10 11:15:29 -0400

    Enable Twig debugging without Drupal console

--- scripts/pre-init.d/90_environment_settings.sh
+++ scripts/pre-init.d/90_environment_settings.sh
@@ -2,10 +2,17 @@
 if [ "$DEPLOY_ENV" == "local" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes en devel field_ui views_ui
   mkdir -p ${DRUPAL_BUILD_TMPROOT}/modules/contrib
+
+  # Enable Drupal Console
   cd ${DRUPAL_ROOT}
   drupal init -n
-  drupal site:mode dev
-  sed -i "s|twig.config:.*|twig.config: { debug: true, auto_reload: null, cache: false }|g" ${DRUPAL_ROOT}/sites/default/services.yml
+
+  # Copy default services
+  cp ${DRUPAL_ROOT}/sites/default/default.services.yml ${DRUPAL_ROOT}/sites/default/services.yml
+
+  # Twig settings
+  sed -i "s|debug: false|debug: true|g" ${DRUPAL_ROOT}/sites/default/services.yml
+  sed -i "s|cache: true|cache: false|g" ${DRUPAL_ROOT}/sites/default/services.yml
 elif [ "$DEPLOY_ENV" == "prod" ]; then
   drush --root=${DRUPAL_ROOT} --uri=default --yes pm-uninstall devel field_ui views_ui dblog
 fi
