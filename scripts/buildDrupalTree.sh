f4780d9dc4648875859266416f052ea8fd3a7760
Jacob Sanford <jsanford@unb.ca>
2017-04-19 15:16:49 -0300

    Remove install config, copy from current core

--- scripts/buildDrupalTree.sh
+++ scripts/buildDrupalTree.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env sh
+
+# Copy build files into a temporary build location.
 mkdir ${DRUPAL_BUILD_TMPROOT}
 cp ${TMP_DRUPAL_BUILD_DIR}/composer.json ${DRUPAL_BUILD_TMPROOT}
 cp -r ${TMP_DRUPAL_BUILD_DIR}/scripts ${DRUPAL_BUILD_TMPROOT}
-cp -r ${TMP_DRUPAL_BUILD_DIR}/drush ${DRUPAL_BUILD_TMPROOT}
 cd ${DRUPAL_BUILD_TMPROOT}
-composer install --${DRUPAL_COMPOSER_DEV}
-ln -s /app/html/vendor/bin/drush /usr/bin/drush
-ln -s /app/html/vendor/bin/drupal /usr/bin/drupal
+
+# Build instance.
+composer install --${COMPOSER_DEPLOY_DEV}
+
+# Configure scaffolding files.
+ln -s ${DRUPAL_BUILD_TMPROOT}/vendor/bin/drush /usr/bin/drush
+ln -s ${DRUPAL_BUILD_TMPROOT}/vendor/bin/drupal /usr/bin/drupal
+
+# Move profile from repo to build root.
 mv ${TMP_DRUPAL_BUILD_DIR}/${DRUPAL_SITE_ID} ${DRUPAL_BUILD_TMPROOT}/profiles/
+
+# Copy config from standard install profile for current version of Drupal.
+cp -r ${DRUPAL_BUILD_TMPROOT}/core/profiles/standard/config ${DRUPAL_BUILD_TMPROOT}/profiles/${DRUPAL_SITE_ID}/
+
+# Move settings files into build location.
 mkdir -p ${DRUPAL_BUILD_TMPROOT}/sites/all
 mv ${TMP_DRUPAL_BUILD_DIR}/settings ${DRUPAL_BUILD_TMPROOT}/sites/all/
