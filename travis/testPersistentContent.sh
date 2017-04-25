6a7ae866e1fa26acad8500cd82338595875f565f
Jacob Sanford <jsanford@unb.ca>
2017-04-25 15:37:02 -0400

    Move travis testing upstream and pull from repo

--- travis/testPersistentContent.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -I --fail http://127.0.0.1/node/5
--- travis/triggerDockerHubBuild.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Trigger an image's build on dockerhub. DOCKER_HUB_TRIGGER_TOKEN must be set
-# in the Repository's "Environment Variables" on travis-ci.org.
-DOCKERHUB_POST_DATA="{\"docker_tag\": \"${DOCKER_IMAGE_TAG}\"}"
-DOCKERHUB_POST_URI="https://registry.hub.docker.com/u/${DOCKER_IMAGE_NAME}/trigger/${DOCKER_HUB_TRIGGER_TOKEN}/"
-
-curl -H "Content-Type: application/json" --data "${DOCKERHUB_POST_DATA}" -X POST "${DOCKERHUB_POST_URI}"
