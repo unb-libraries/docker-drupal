14e3ab9442bb19b40c52ad3078ce705b9d68c888
Jacob Sanford <jsanford@unb.ca>
2017-06-22 11:09:11 -0300

    Ensure local development instances have proper file permissions

--- scripts/pre-init.d/70_set_permissions.sh
+++ scripts/pre-init.d/70_set_permissions.sh
@@ -2,5 +2,10 @@
 # Prevent web daemon from modifying drupal tree files.
 find ${DRUPAL_ROOT} -not -path "${DRUPAL_ROOT}/sites/default/*" -exec chown root:root {} \;
 
+# Make sure local development sets file permissions.
+if [ "$DEPLOY_ENV" == "local" ]; then
+  chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} -R ${DRUPAL_ROOT}/sites/default/files
+fi
+
 # Prevent web daemon from changing files .htaccess
 chown root:root ${DRUPAL_ROOT}/sites/default/files/.htaccess
