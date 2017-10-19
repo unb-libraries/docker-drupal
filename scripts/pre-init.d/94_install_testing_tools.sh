29e4015ed0eef487593d70d776862cbcc07ce871
Jacob Sanford <jsanford@unb.ca>
2017-10-19 12:02:20 -0300

    Update legacy reference to DRUPAL_BEHAT_TESTING_ROOT

--- scripts/pre-init.d/94_install_testing_tools.sh
+++ scripts/pre-init.d/94_install_testing_tools.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 if [ "$DRUPAL_TESTING_TOOLS" != "FALSE" ]; then
-  cd ${DRUPAL_BEHAT_TESTING_ROOT}
+  cd ${DRUPAL_TESTING_ROOT}
   composer install --prefer-dist
   ./vendor/bin/behat --init
-  chmod -R g+w ${DRUPAL_BEHAT_TESTING_ROOT}
 fi
