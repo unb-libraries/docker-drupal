11585820441659896a67ec2ebaf41b50dbd6f2d0
Jacob Sanford <jsanford@unb.ca>
2017-11-09 15:09:11 -0400

    Update runTests.sh

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -2,4 +2,11 @@
 
 # Behat.
 cd "${DRUPAL_TESTING_ROOT}/behat"
+
+# If we have mounted in the test dir as a volume, the vendor may have been overwritten.
+if [ ! -e "./vendor/bin/behat" ]; then
+  echo "File does not exist"
+  composer install
+fi 
+
 ./vendor/bin/behat
