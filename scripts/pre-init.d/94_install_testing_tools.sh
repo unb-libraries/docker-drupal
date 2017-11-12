4be922658aaca579f758f8ff301dc062ccf0e766
Jacob Sanford <jsanford@unb.ca>
2017-11-12 16:24:21 -0400

    Properly test for absence of file

--- scripts/pre-init.d/94_install_testing_tools.sh
+++ scripts/pre-init.d/94_install_testing_tools.sh
@@ -3,7 +3,7 @@ if [ "$DRUPAL_TESTING_TOOLS" != "FALSE" ]; then
   cd ${DRUPAL_TESTING_ROOT}/behat
 
   # Avoid case where user mounts testing in compose and nukes vendor.
-  if [ -f ./vendor/bin/behat ]; then
+  if [ ! -f ./vendor/bin/behat ]; then
     echo "Installing behat testing libraries"
     composer install --prefer-dist --no-interaction --dev
   fi
