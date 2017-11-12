1302aee2046c289d1538c4bcb459790ff0d09d71
Jacob Sanford <jsanford@unb.ca>
2017-11-12 16:23:32 -0400

    Avoid case where user mounts testing in compose and nukes vendor.

--- scripts/pre-init.d/94_install_testing_tools.sh
+++ scripts/pre-init.d/94_install_testing_tools.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 if [ "$DRUPAL_TESTING_TOOLS" != "FALSE" ]; then
-  # Behat needs a functional drupal install to init.
   cd ${DRUPAL_TESTING_ROOT}/behat
+
+  # Avoid case where user mounts testing in compose and nukes vendor.
+  if [ -f ./vendor/bin/behat ]; then
+    echo "Installing behat testing libraries"
+    composer install --prefer-dist --no-interaction --dev
+  fi
+
+  # Behat needs a functional drupal install to init.
   ./vendor/bin/behat --init
 fi
