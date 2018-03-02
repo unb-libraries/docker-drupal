617021bb839291289e7c1bc8d807dd06d55af18e
Jacob Sanford <jsanford@unb.ca>
2018-03-02 14:56:35 -0400

    Run simpletests when testing

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -7,6 +7,17 @@ cd "${DRUPAL_TESTING_ROOT}/behat"
 if [ ! -e "./vendor/bin/behat" ]; then
   echo "File does not exist"
   composer install
-fi 
+fi
 
 ./vendor/bin/behat
+
+# PHP unit tests
+set -e
+EXEC_DRUSH="drush --yes --root=/app/html --uri=default"
+
+$EXEC_DRUSH en simpletest
+for CUR_TEST_CLASS in $DRUPAL_UNIT_TEST_CLASSES
+do
+   su nginx -s /bin/sh -c "php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class '$CUR_TEST_CLASS'"
+done
+$EXEC_DRUSH pm-uninstall simpletest
