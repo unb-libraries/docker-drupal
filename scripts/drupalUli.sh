26e96cf6cd7ea53b7ae64f31e4fccce443b16601
jsanford <jsanford@unb.ca>
2019-02-25 15:36:58 -0400

    DOCKW-2 Insert local dev URI if it exists

--- scripts/drupalUli.sh
+++ scripts/drupalUli.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env sh
+# Get ULI Output.
 if [[ -z "$1" ]]; then
-  drush --root=${DRUPAL_ROOT} --uri=default --yes --no-browser user:login
+  ULI_OUTPUT=$(drush --root=${DRUPAL_ROOT} --uri=default --yes --no-browser user:login)
 else
-  drush --root=${DRUPAL_ROOT} --uri=default --yes --no-browser user:login --name="$1"
+  ULI_OUTPUT=$(drush --root=${DRUPAL_ROOT} --uri=default --yes --no-browser user:login --name="$1")
+fi
+
+# Substitute in local URI if it exists. Otherwise, output link with 'default'.
+if [[ -z "$DEV_WEB_URI" ]]; then
+  echo "$ULI_OUTPUT"
+else
+  echo "$ULI_OUTPUT" | sed -e "s|http://default|$DEV_WEB_URI|g"
 fi
