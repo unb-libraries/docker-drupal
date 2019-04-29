870be45d14957e7a2e416ae341aca800b3c8472b
jsanford <jsanford@unb.ca>
2019-04-29 14:21:09 -0300

    Increase verbosity when it matters

--- scripts/pre-init.d/90_environment_settings.sh
+++ scripts/pre-init.d/90_environment_settings.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env sh
 if [ "$DEPLOY_ENV" == "local" ]; then
+  echo "Ensuring some modules are enabled for local development..."
   ${DRUSH} en devel field_ui views_ui
   mkdir -p ${DRUPAL_BUILD_TMPROOT}/modules/contrib
 
   # Enable Drupal Console
   cd ${DRUPAL_BUILD_TMPROOT}
-  drupal init -n
+  echo "Enabling Drupal console..."
+  drupal --quiet init -n
 
 elif [ "$DEPLOY_ENV" == "prod" ]; then
+  echo "Ensuring some modules are disabled in production..."
   ${DRUSH} pm-uninstall devel field_ui views_ui dblog  > /dev/null 2>&1
 fi
