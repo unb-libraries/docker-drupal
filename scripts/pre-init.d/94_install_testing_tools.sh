49dc62ba4528887bbece3b32ebbd7e2b801745f3
jsanford <jsanford@unb.ca>
2019-04-29 13:50:40 -0300

    Avoid double-installing testing tools

--- scripts/pre-init.d/94_install_testing_tools.sh
+++ scripts/pre-init.d/94_install_testing_tools.sh
@@ -2,12 +2,6 @@
 if [ "$DRUPAL_TESTING_TOOLS" != "FALSE" ]; then
   cd ${DRUPAL_TESTING_ROOT}/behat
 
-  # Avoid case where user mounts testing in compose and nukes vendor.
-  if [ ! -f ./vendor/bin/behat ]; then
-    echo "Installing behat testing libraries"
-    composer install --prefer-dist --no-interaction --dev
-  fi
-
   # Behat needs a functional drupal install to init.
   ./vendor/bin/behat --init
 fi
