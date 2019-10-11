1a2b73e5776be4e4c5068eb42a5d3f68016a7e55
jsanford <jsanford@unb.ca>
2019-10-11 07:57:31 -0300

    Add PHPUnit tests to runTests.sh

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -8,6 +8,14 @@ if [ -d "${DRUPAL_TESTING_ROOT}/behat" ]; then
     echo "Installing behat testing tools..."
     composer install --prefer-dist
   fi
-
   ./vendor/bin/behat
 fi
+
+# PHPunit.
+EXEC_DRUSH="drush --yes --root=/app/html --uri=default"
+$EXEC_DRUSH en simpletest
+for CUR_TEST_CLASS in $DRUPAL_UNIT_TEST_CLASSES
+do
+   su nginx -s /bin/sh -c "php /app/html/core/scripts/run-tests.sh --url http://127.0.0.1 --php /usr/bin/php --die-on-fail --class '$CUR_TEST_CLASS'"
+done
+$EXEC_DRUSH pm-uninstall simpletest
