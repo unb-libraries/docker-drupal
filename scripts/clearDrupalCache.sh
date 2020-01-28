358cfb84d41668d7ca568de8ef8c19e3405023b2
Jacob Sanford <jsanford@unb.ca>
2020-01-28 15:04:22 -0400

    Also cache-clear with drush

--- scripts/clearDrupalCache.sh
+++ scripts/clearDrupalCache.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 echo "SHOW TABLES LIKE 'cache%'" | $(drush sql-connect) | tail -n +2 | xargs -n1 -I% echo "TRUNCATE TABLE %;" | $(drush sql-connect)
 echo "Drupal Cache Tables Truncated."
+$DRUSH cr
