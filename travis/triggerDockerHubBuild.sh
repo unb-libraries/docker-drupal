f06aa45933b6c54caa5be4311dd76ed6c08bd0ed
Jacob Sanford <jsanford@unb.ca>
2020-02-03 13:54:36 -0400

    Fix dockerhub build trigger in travis

--- travis/triggerDockerHubBuild.sh
+++ travis/triggerDockerHubBuild.sh
@@ -2,6 +2,4 @@
 # Trigger an image's build on dockerhub. DOCKER_HUB_TRIGGER_TOKEN must be set
 # in the Repository's "Environment Variables" on travis-ci.org.
 DOCKERHUB_POST_DATA="{\"docker_tag\": \"${DOCKER_IMAGE_TAG}\"}"
-DOCKERHUB_POST_URI="https://registry.hub.docker.com/u/${DOCKER_IMAGE_NAME}/trigger/${DOCKER_HUB_TRIGGER_TOKEN}/"
-
-curl -H "Content-Type: application/json" --data "${DOCKERHUB_POST_DATA}" -X POST "${DOCKERHUB_POST_URI}"
+curl -H "Content-Type: application/json" --data "${DOCKERHUB_POST_DATA}" -X POST "${DOCKER_HUB_TRIGGER_URL}"
