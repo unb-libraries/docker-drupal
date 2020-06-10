f5e0d86a7591b9bf8d6c5a36744dbd3f54b1a7d7
Jacob Sanford <jsanford@unb.ca>
2020-06-10 09:49:14 -0300

    Avoid erroring on empty permissions lists

--- scripts/pre-init.d/71_set_config_sync_permissions.sh
+++ scripts/pre-init.d/71_set_config_sync_permissions.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 # Allow the web daemon user to write to the config sync directory.
-find ${DRUPAL_ROOT}/config/sync \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -0 chown ${NGINX_RUN_USER}:${NGINX_RUN_USER} --
+find ${DRUPAL_ROOT}/config/sync \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_USER} --
