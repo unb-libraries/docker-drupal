a0dbfca5da56bdf202e979c2a8f1716e68f57527
Jacob Sanford <jsanford@unb.ca>
2020-06-11 14:44:59 -0300

    Change permissions to user:group not user:user

--- scripts/pre-init.d/71_set_public_file_permissions.sh
+++ scripts/pre-init.d/71_set_public_file_permissions.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-find ${DRUPAL_ROOT}/sites/default/files \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_USER} --
+find ${DRUPAL_ROOT}/sites/default/files \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} --
 
 # Exception : prevent the web daemon user from changing files directory .htaccess
 chown root:root ${DRUPAL_ROOT}/sites/default/files/.htaccess
