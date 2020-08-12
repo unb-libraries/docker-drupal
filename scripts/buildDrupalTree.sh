92007e8b63c785c1ad5fa4a67202e82271445881
Jacob Sanford <jsanford@unb.ca>
2020-08-12 11:19:50 -0300

    Ensure sites/default does not get clobbered by volume on new installs

--- scripts/buildDrupalTree.sh
+++ scripts/buildDrupalTree.sh
@@ -1,42 +1,57 @@
 #!/usr/bin/env sh
-YQ_VERSION="2.4.1"
+set -e
 
-# Install prestissimo.
-composer global require --prefer-dist hirak/prestissimo
+# Profile ID
+DRUPAL_BASE_PROFILE="${1:-minimal}"
+
+# Set-up Composer
+cp /build/composer.json ${DRUPAL_ROOT}
+cd ${DRUPAL_ROOT}
 
 # Get latest composer/ScriptHandler.php.
-rm -rf "${DRUPAL_ROOT}"
-mkdir -p "${DRUPAL_ROOT}/scripts/composer"
-curl -o "${DRUPAL_ROOT}/scripts/composer/ScriptHandler.php" -O https://raw.githubusercontent.com/drupal-composer/drupal-project/8.x/scripts/composer/ScriptHandler.php
+mkdir -p scripts/composer
+curl -O https://raw.githubusercontent.com/drupal-composer/drupal-project/8.x/scripts/composer/ScriptHandler.php
+mv ScriptHandler.php scripts/composer/
 
-# Build instance.
-cd "${DRUPAL_ROOT}"
-cp /build/composer.json .
-BUILD_COMMAND="composer install --no-ansi --prefer-dist --no-dev"
-echo "Building - $BUILD_COMMAND"
-$BUILD_COMMAND
+# Build application.
+BUILD_COMMAND="composer install --no-suggest --prefer-dist --no-interaction --no-progress --no-dev"
+echo "Updating Drupal [${BUILD_COMMAND}]"
+${BUILD_COMMAND}
 
-# Move custom install profile from build to webtree.
-mv /build/${DRUPAL_SITE_ID} ${DRUPAL_ROOT}/profiles/
+# Backup the settings directory for use with a fresh install.
+cp -r /app/html/sites/default /tmp/
 
-# Copy config from standard install profile to custom profile config.
-cp -r ${DRUPAL_ROOT}/core/profiles/standard/config ${DRUPAL_ROOT}/profiles/${DRUPAL_SITE_ID}/
+# Create the profile folder.
+mkdir -p "${DRUPAL_ROOT}/profiles/${DRUPAL_SITE_ID}"
 
-# Move settings files into build location.
-mkdir -p ${DRUPAL_ROOT}/sites/all
-mv /build/settings ${DRUPAL_ROOT}/sites/all/
+# Copy config from core install profile for current version of Drupal.
+rsync -a --inplace --no-compress ${RSYNC_FLAGS} "${DRUPAL_ROOT}/core/profiles/${DRUPAL_BASE_PROFILE}/config" "${DRUPAL_ROOT}/profiles/${DRUPAL_SITE_ID}/"
+
+# Copy additional configs provided by this extension.
+ADDITIONAL_CONFIG_DIR="/scripts/data/profiles/${DRUPAL_BASE_PROFILE}/config"
+if [[ -d "$ADDITIONAL_CONFIG_DIR" ]]; then
+  rsync -a --inplace --no-compress ${RSYNC_FLAGS} ${ADDITIONAL_CONFIG_DIR} ${DRUPAL_ROOT}/profiles/${DRUPAL_SITE_ID}/
+fi
+
+# Move local profile from repo to webroot, overwriting.
+rsync -a --inplace --no-compress ${RSYNC_FLAGS} --remove-source-files /build/${DRUPAL_SITE_ID} ${DRUPAL_ROOT}/profiles/
+
+# Move settings files into webroot.
+rsync -a --inplace --no-compress ${RSYNC_FLAGS} --remove-source-files /build/settings ${DRUPAL_ROOT}/sites/all/
+
+# Add drush and drupal console PATH locations.
+ln -s ${DRUPAL_ROOT}/vendor/bin/drush /usr/bin/drush
+ln -s ${DRUPAL_ROOT}/vendor/bin/drupal /usr/bin/drupal
 
 # Set default permissions.
-find ${DRUPAL_ROOT} \! -user root \! -group root -not \( -path "${DRUPAL_ROOT}/sites/default/files" -prune \) -print0 | xargs -r0 chown root:root --
-
-# Install yq for yaml combination scripts.
-echo "Downloading yq binary..."
-curl -sL https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/yq_linux_amd64 -o /usr/bin/yq; chmod +x /usr/bin/yq
+find ${DRUPAL_ROOT} -not \( -path "${DRUPAL_ROOT}/sites/default/files" -prune \) -not -user root -not -group root -print0 | xargs -r0 chown root:root --
 
 # Ensure the configuration sync directory exists.
 mkdir -p ${DRUPAL_ROOT}/config/sync
 chown ${NGINX_RUN_USER}:${NGINX_RUN_USER} ${DRUPAL_ROOT}/config/sync
 
 # Move services to /app/services.
-mkdir -p /app/services
-cp -r /build/services/* /app/services/
+if [ -d "/build/services" ]; then
+  cp -rf /build/services/* /app/services/
+fi
+
--- scripts/pre-init.d/60_install_drupal.sh
+++ scripts/pre-init.d/60_install_drupal.sh
@@ -2,10 +2,15 @@
 # Check if this is a new deployment. If so, install.
 if [[ ! -f /tmp/DRUPAL_DB_LIVE && ! -f /tmp/DRUPAL_FILES_LIVE ]];
 then
-  # Create Database.
+  # Deploy the sites/default tree likely removed by the volume.
+  rsync -a /tmp/default /app/html/sites/
+  chown -R ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} /app/html/sites/default
+  chmod +w /app/html/sites/default/settings.php
+
+  # Create the database.
   mysql -uroot -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOSTNAME} -P ${MYSQL_PORT} -e "DROP DATABASE IF EXISTS ${DRUPAL_SITE_ID}_db; CREATE DATABASE ${DRUPAL_SITE_ID}_db CHARACTER SET utf8 COLLATE utf8_general_ci; CREATE USER '${DRUPAL_SITE_ID}_user'@'%' IDENTIFIED BY '$DRUPAL_DB_PASSWORD'; GRANT ALL PRIVILEGES ON ${DRUPAL_SITE_ID}_db.* TO '${DRUPAL_SITE_ID}_user'@'%' IDENTIFIED BY '$DRUPAL_DB_PASSWORD'; FLUSH PRIVILEGES;"
 
-  # Perform site-install.
+  # Perform a drush site-install.
   cd ${DRUPAL_ROOT}
   /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" ${DRUSH} site-install "${DRUPAL_SITE_ID}" --verbose --account-name="${DRUPAL_ADMIN_ACCOUNT_NAME}" --account-pass="${DRUPAL_ADMIN_ACCOUNT_PASS}" --db-url="mysqli://${DRUPAL_SITE_ID}_user:$DRUPAL_DB_PASSWORD@${MYSQL_HOSTNAME}:${MYSQL_PORT}/${DRUPAL_SITE_ID}_db"
 fi
