0c059fd1a3ee004324d5c359cf4874b53e74a1ad
Jacob Sanford <jsanford@unb.ca>
2020-08-20 15:16:49 -0300

    Install testing tools before running PHPUnit tests

--- scripts/runUnitTests.sh
+++ scripts/runUnitTests.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
+if [ ! -e "${DRUPAL_ROOT}/vendor/bin/phpunit" ]; then
+  echo "Installing PHPUnit testing tools..."
+  cd ${DRUPAL_ROOT}
+  ${COMPOSER_INSTALL}
+fi
+
 # PHPUnit Tests
 for CUR_TEST_MODULE in $DRUPAL_UNIT_TEST_MODULES
 do
