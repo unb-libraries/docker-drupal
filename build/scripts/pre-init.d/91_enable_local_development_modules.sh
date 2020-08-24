2ab6492975b86e5c00aafcaa8b42eceb90d51067
Jacob Sanford <jsanford@unb.ca>
2020-08-24 11:33:37 -0300

    DOCKW-25 Enable development mode and modules when local

---
+++ build/scripts/pre-init.d/91_enable_local_development_modules.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+if [ "$DEPLOY_ENV" == "local" ]; then
+  echo "Local environment detected, installing and enabling development modules..."
+  cd ${DRUPAL_ROOT}
+  ${COMPOSER_INSTALL}
+  ${DRUSH} en devel
+  drupal site:mode dev
+fi
