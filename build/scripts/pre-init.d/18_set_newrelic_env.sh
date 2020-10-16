67dadd14013527203f8118f2a0a19b879fd27d1e
Jacob Sanford <jsanford@unb.ca>
2020-10-16 13:27:46 -0300

    Add labels instead of strange app names to newrelic

--- build/scripts/pre-init.d/18_set_newrelic_env.sh
+++ build/scripts/pre-init.d/18_set_newrelic_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 if [ -n "$DEPLOY_ENV" ] && [ -n "$NR_INSTALL_KEY" ] && [ ! "$DEPLOY_ENV" = "local" ]; then
-  sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"${DRUPAL_SITE_URI};${DEPLOY_ENV};${DRUPAL_SITE_URI}_${DEPLOY_ENV}\"|g" /etc/php7/conf.d/newrelic.ini
+  sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"${DRUPAL_SITE_URI}\"|g" /etc/php7/conf.d/newrelic.ini
   sed -i "s|REPLACE_WITH_REAL_KEY|${NR_INSTALL_KEY}|g" /etc/php7/conf.d/newrelic.ini
+  echo "newrelic.labels = \"Environment:${DEPLOY_ENV};Node:${KUBERNETES_SERVICE_HOST};Framework:Drupal\"" >> /etc/php7/conf.d/newrelic.ini
 fi
