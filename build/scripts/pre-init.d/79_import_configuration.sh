14f2a32d4aca93338d7961e8c1ae29ff79b39059
Jacob Sanford <jsanford@unb.ca>
2021-04-13 14:54:31 -0300

    Run config import twice on deploy.
    See https://github.com/drush-ops/drush/issues/2449 for rationale.

--- build/scripts/pre-init.d/79_import_configuration.sh
+++ build/scripts/pre-init.d/79_import_configuration.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 # Configuration
 if [ -d "$DRUPAL_CONFIGURATION_DIR" ] && [ "$(ls $DRUPAL_CONFIGURATION_DIR)" ]; then
+  /scripts/configImport.sh || /scripts/configImport.sh
   /scripts/configImport.sh
 fi
