5d96b2ac21dec9c767a10ed409fbdfbdceef8491
Jacob Sanford <jsanford@unb.ca>
2021-04-13 15:28:00 -0300

    Update script docblock

--- build/scripts/flushRedisCache.sh
+++ build/scripts/flushRedisCache.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+# This script flushes the redis cache for the instance prefix only.
 CACHE_PREFIX=$(drush eval 'echo \Drupal\Core\Site\Settings::get("cache_prefix")["default"]')
 if [ -n "$CACHE_PREFIX" ]; then
   redis-cli -h drupal-redis-lib-unb-ca EVAL "for _,k in ipairs(redis.call('keys', ARGV[1])) do redis.call('del',k) end" 0 "$CACHE_PREFIX:*:*"
