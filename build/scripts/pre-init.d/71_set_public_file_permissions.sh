8bd761ddf8af384d770f412c0ee21335365edb7a
Jacob Sanford <jsanford@unb.ca>
2021-04-29 09:55:22 -0300

    Change filesystem permissions on local

--- build/scripts/pre-init.d/71_set_public_file_permissions.sh
+++ build/scripts/pre-init.d/71_set_public_file_permissions.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # Instances with massive filesystems can choke on this command.
-if [ "$DRUPAL_CHOWN_PUBLIC_FILES_STARTUP" = "TRUE" ]; then
+if [ "$DRUPAL_CHOWN_PUBLIC_FILES_STARTUP" = "TRUE" ] || [ "$DEPLOY_ENV" == "local" ]; then
   find ${DRUPAL_ROOT}/sites/default/files \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} --
 fi
 
