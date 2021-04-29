2abe2b6977c0cf6c579cd2f38a2d87e6565b178d
Jacob Sanford <jsanford@unb.ca>
2021-04-29 09:14:51 -0300

    Avoid slamming filesystem on startup

--- build/scripts/pre-init.d/70_set_drupal_tree_permissions.sh
+++ build/scripts/pre-init.d/70_set_drupal_tree_permissions.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 # Prevent the web daemon user from modifying drupal tree files.
-find ${DRUPAL_ROOT} -not \( -path "${DRUPAL_ROOT}/sites/default/files" -prune \) -not -user root -not -group root -print0 | xargs -r0 chown root:root --
+# Disabled : I believe this is unnecessary.
+# find ${DRUPAL_ROOT} -not \( -path "${DRUPAL_ROOT}/sites/default/files" -prune \) -not -user root -not -group root -print0 | xargs -r0 chown root:root --
