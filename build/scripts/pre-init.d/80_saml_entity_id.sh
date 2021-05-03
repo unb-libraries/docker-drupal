59e416d8a2c4c430149d8fb1a95870938a307f90
Brian Cassidy <brian.cassidy@unb.ca>
2021-05-03 11:10:09 -0300

    fix samlauth check due to the new samlauth_user_fields module

--- build/scripts/pre-init.d/80_saml_entity_id.sh
+++ build/scripts/pre-init.d/80_saml_entity_id.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-SAMLAUTH=$($DRUSH pml --no-core --status=Enabled --format=list|grep samlauth)
+SAMLAUTH=$($DRUSH pml --no-core --status=Enabled --format=list|grep -E '^samlauth$')
 if [ "$DEPLOY_ENV" == "dev" ] && [ "$SAMLAUTH" == "samlauth" ]; then
   ${DRUSH} cset samlauth.authentication sp_entity_id "https://dev-${DRUPAL_SITE_URI}"
 fi
