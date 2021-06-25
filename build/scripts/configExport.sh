3258af289d68bc68ecf23de059ccaf64d8c443f3
Jacob Sanford <jsanford@unb.ca>
2021-06-25 11:10:02 -0300

    Remove deprecated skip-modules from config export

--- build/scripts/configExport.sh
+++ build/scripts/configExport.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 rm -rf ${DRUPAL_CONFIGURATION_DIR}/*
 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} ${DRUPAL_CONFIGURATION_DIR}
-${DRUSH} config-export --skip-modules=${DRUPAL_CONFIGURATION_EXPORT_SKIP} --destination=${DRUPAL_CONFIGURATION_DIR}
+${DRUSH} config-export --destination=${DRUPAL_CONFIGURATION_DIR}
