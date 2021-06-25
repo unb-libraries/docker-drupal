156fb48c14fd81c56dead3879b1a72fa003677af
Jacob Sanford <jsanford@unb.ca>
2021-06-25 10:38:40 -0300

    DOCKW-38 Properly initialize mailhog

--- build/scripts/pre-init.d/19_configure_postfix.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-sed -i "s|APP_HOSTNAME|$APP_HOSTNAME|g" /etc/postfix/main.cf
--- build/scripts/pre-init.d/99_zz_notify_user_URI.sh
+++ build/scripts/pre-init.d/99_zz_notify_user_URI.sh
@@ -5,7 +5,8 @@ if [ -n "$LOCAL_HOSTNAME" ] && [ -n "$LOCAL_PORT" ]; then
 
   printf "\n\nLog-in to your instance via:\n"
   /scripts/drupalUli.sh
-  if nslookup mailhog &> /dev/null; then
+
+  if nc -z mailhog 1025; then
     MAILHOG_PORT=$((LOCAL_PORT+1000))
     printf "\nView sent mail at:\n"
     echo "http://$LOCAL_HOSTNAME:$MAILHOG_PORT"
