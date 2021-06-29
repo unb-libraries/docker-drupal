3e6b4023b74679cbd7fcd9ac525501745e8205e4
Jacob Sanford <jsanford@unb.ca>
2021-06-29 19:15:44 -0300

    Write devel yaml values to scaffold

--- build/scripts/pre-init.d/91_enable_local_development_modules.sh
+++ build/scripts/pre-init.d/91_enable_local_development_modules.sh
@@ -2,16 +2,21 @@
 if [ "$DEPLOY_ENV" == "local" ]; then
   echo "Local environment detected, installing and enabling development modules..."
 
+  # Set dev container settings in scaffold files.
+  yq w -i ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml parameters.[http.response.debug_cacheability_headers] true
+  yq w -i ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml parameters.[twig.config].auto_reload true
+  yq w -i ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml parameters.[twig.config].debug true
+  yq w -i ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml parameters.[twig.config].cache false
+  cp ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml ${DRUPAL_ROOT}/sites/development.services.yml
+
   # Install composer dev packages.
   cd ${DRUPAL_ROOT}
-  ${COMPOSER_INSTALL}
-  ${DRUSH} en devel
 
-  # Set dev container settings.
-  yq w -i ${DRUPAL_ROOT}/sites/development.services.yml parameters.[http.response.debug_cacheability_headers] true
-  yq w -i ${DRUPAL_ROOT}/sites/development.services.yml parameters.[twig.config].auto_reload true
-  yq w -i ${DRUPAL_ROOT}/sites/development.services.yml parameters.[twig.config].debug true
-  yq w -i ${DRUPAL_ROOT}/sites/development.services.yml parameters.[twig.config].cache false
+  # This composer install command also copies scaffold development.services.yml.
+  ${COMPOSER_INSTALL}
+
+  # Enable devel.
+  ${DRUSH} en devel
 
   # Make sure caching is disabled.
   echo "\$settings['cache']['bins']['render'] = 'cache.backend.null';" >> ${DRUPAL_ROOT}/sites/all/settings/settings.local.inc
