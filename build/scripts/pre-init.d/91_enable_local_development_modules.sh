31ea321c8cd5a52dc8827f8e0ebef2d7f0b69954
Jacob Sanford <jsanford@unb.ca>
2021-06-29 15:47:18 -0300

    Remove dependence on drupal console in startup

--- build/scripts/pre-init.d/91_enable_local_development_modules.sh
+++ build/scripts/pre-init.d/91_enable_local_development_modules.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env sh
 if [ "$DEPLOY_ENV" == "local" ]; then
   echo "Local environment detected, installing and enabling development modules..."
+
+  # Install composer dev packages.
   cd ${DRUPAL_ROOT}
   ${COMPOSER_INSTALL}
   ${DRUSH} en devel
+
+  # Set dev container settings.
+  yq w -i /app/html/sites/default/services.yml parameters.[http.response.debug_cacheability_headers] true
+  yq w -i /app/html/sites/default/services.yml parameters.[twig.config].auto_reload true
+  yq w -i /app/html/sites/default/services.yml parameters.[twig.config].auto_debug true
+  yq w -i /app/html/sites/default/services.yml parameters.[twig.config].auto_cache false
+
+  # Make sure caching is disabled.
+  echo "\$settings['cache']['bins']['render'] = 'cache.backend.null';" >> ${DRUPAL_ROOT}/sites/all/settings/settings.local.inc
+  echo "\$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';" >> ${DRUPAL_ROOT}/sites/all/settings/settings.local.inc
 fi
