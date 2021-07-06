8576ea90ac2f267c6ccba604af7124c389fd81f7
Jacob Sanford <jsanford@unb.ca>
2021-07-06 09:12:30 -0300

    Optimize table truncation in cache clearing

--- build/scripts/truncateDrupalCacheTables.sh
+++ build/scripts/truncateDrupalCacheTables.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 #
 # Forcibly truncate all cache tables.
-drush sql-query "SELECT table_name FROM information_schema.tables WHERE table_name LIKE 'cache\_%';" | while read -r LINE ; do
+drush sql-query "SELECT table_name FROM information_schema.tables WHERE table_name LIKE 'cache\_%';" | ( while read -r LINE ; do
   if [ -n "$LINE" ]; then
-    COMMAND="TRUNCATE TABLE $LINE"
-    echo "Truncating table [$LINE]..."
-    drush sql-query "$COMMAND" 1> /dev/null
+    echo "Found table $LINE..."
+    COMMAND="${COMMAND} TRUNCATE TABLE $LINE;"
   fi
 done
+$DRUSH sqlq "$COMMAND" && echo "Tables Truncated!" )
