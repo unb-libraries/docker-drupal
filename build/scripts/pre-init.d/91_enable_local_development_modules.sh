16f968172b7ef04eb0dcbf830c14998bfeaa05ea
Jacob Sanford <jsanford@unb.ca>
2021-07-27 12:56:04 -0300

    Ensure yq combines core assets

--- build/scripts/pre-init.d/91_enable_local_development_modules.sh
+++ build/scripts/pre-init.d/91_enable_local_development_modules.sh
@@ -3,11 +3,11 @@ if [ "$DEPLOY_ENV" == "local" ]; then
   echo "Local environment detected, installing and enabling development modules..."
 
   # Set dev container settings in scaffold files.
-  yq w -i ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml parameters.[http.response.debug_cacheability_headers] true
-  yq w -i ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml parameters.[twig.config].auto_reload true
-  yq w -i ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml parameters.[twig.config].debug true
-  yq w -i ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml parameters.[twig.config].cache false
-  cp ${DRUPAL_ROOT}/assets/scaffold/files/development.services.yml ${DRUPAL_ROOT}/sites/development.services.yml
+  yq w -i ${DRUPAL_ROOT}/core/assets/scaffold/files/development.services.yml parameters.[http.response.debug_cacheability_headers] true
+  yq w -i ${DRUPAL_ROOT}/core/assets/scaffold/files/development.services.yml parameters.[twig.config].auto_reload true
+  yq w -i ${DRUPAL_ROOT}/core/assets/scaffold/files/development.services.yml parameters.[twig.config].debug true
+  yq w -i ${DRUPAL_ROOT}/core/assets/scaffold/files/development.services.yml parameters.[twig.config].cache false
+  cp ${DRUPAL_ROOT}/core/assets/scaffold/files/development.services.yml ${DRUPAL_ROOT}/sites/development.services.yml
 
   # Install composer dev packages.
   cd ${DRUPAL_ROOT}
