f69ff0d4ad1d62248ff91baff31efc39eae7ec54
Jacob Sanford <jsanford@unb.ca>
2021-08-26 09:46:32 -0300

    Avoid echoing multiple times in cache clear truncation

--- build/scripts/truncateDrupalCacheTables.sh
+++ build/scripts/truncateDrupalCacheTables.sh
@@ -3,8 +3,8 @@
 # Forcibly truncate all cache tables.
 drush sql-query "SELECT table_name FROM information_schema.tables WHERE table_name LIKE 'cache\_%';" | ( while read -r LINE ; do
   if [ -n "$LINE" ]; then
-    echo "Found table $LINE..."
     COMMAND="${COMMAND} TRUNCATE TABLE $LINE;"
   fi
 done
+echo "Executing $COMMAND..."
 $DRUSH sqlq "$COMMAND" && echo "Tables Truncated!" )
