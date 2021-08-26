31d96d229aecc94609021890342069de0d704e74
Jacob Sanford <jsanford@unb.ca>
2021-08-26 09:31:07 -0300

    Explain why we import configuration 3 times

--- build/scripts/pre-init.d/79_import_configuration.sh
+++ build/scripts/pre-init.d/79_import_configuration.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 # Configuration
 if [ -d "$DRUPAL_CONFIGURATION_DIR" ] && [ "$(ls $DRUPAL_CONFIGURATION_DIR)" ]; then
+  # What is this monstrosity? See https://github.com/drush-ops/drush/issues/2449.
   /scripts/configImport.sh || /scripts/configImport.sh
   /scripts/configImport.sh
 fi
