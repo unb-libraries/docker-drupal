fe7c97ad0336cf20dfb83bb5eb39034d968ae13a
Jacob Sanford <jsanford@unb.ca>
2021-08-27 09:02:49 -0300

    Make sure the config sync dir is writable by the webserver

--- build/scripts/pre-init.d/71_set_public_file_permissions.sh
+++ build/scripts/pre-init.d/71_set_public_file_permissions.sh
@@ -4,5 +4,8 @@ if [ "$DRUPAL_CHOWN_PUBLIC_FILES_STARTUP" = "TRUE" ] || [ "$DEPLOY_ENV" == "loca
   find ${DRUPAL_ROOT}/sites/default/files \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} --
 fi
 
+# Make sure the config sync dir is writable by the webserver.
+chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} "$DRUPAL_CONFIGURATION_DIR"
+
 # Exception : prevent the web daemon user from changing files directory .htaccess
 chown root:root ${DRUPAL_ROOT}/sites/default/files/.htaccess
