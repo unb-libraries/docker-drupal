898c0fd99ff2e8d81a302e33a7081e138c085a4e
Brian Cassidy <brian.cassidy@unb.ca>
2021-09-09 14:55:28 -0300

    simplify samlauth identity update

--- build/scripts/pre-init.d/80_saml_entity_id.sh
+++ build/scripts/pre-init.d/80_saml_entity_id.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-SAMLAUTH=$($DRUSH pml --no-core --status=Enabled --format=list|grep samlauth)
-if [ "$DEPLOY_ENV" == "dev" ] && [ "$SAMLAUTH" == "samlauth" ]; then
-  ${DRUSH} cset samlauth.authentication sp_entity_id "https://dev-${DRUPAL_SITE_URI}"
+if [ "$DEPLOY_ENV" == "dev" ]; then
+  $DRUSH config-get samlauth.authentication && ${DRUSH} cset samlauth.authentication sp_entity_id "https://dev-${DRUPAL_SITE_URI}"
 fi
