8c88930bdfb45391fe75e3cc3af37a6c1728fc2c
Jacob Sanford <jsanford@unb.ca>
2021-10-14 10:33:42 -0300

    Deny access to configuration directory, even though it is outside webtree

--- build/scripts/pre-init.d/71_set_config_sync_permissions.sh
+++ build/scripts/pre-init.d/71_set_config_sync_permissions.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
 # Allow the web daemon user to write to the config sync directory.
-find ${DRUPAL_ROOT}/config/sync \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} --
+find ${DRUPAL_CONFIGURATION_DIR} \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} --
+
+# Add a .htaccess file that denies execution to satisfy Drupal.
+cp /security_htaccess/.htaccess.PreventExecution ${DRUPAL_CONFIGURATION_DIR}/.htaccess.PreventExecution
+chown root:root ${DRUPAL_CONFIGURATION_DIR}/.htaccess.PreventExecution
