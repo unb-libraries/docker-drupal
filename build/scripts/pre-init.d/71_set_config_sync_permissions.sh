44659f90a90ef7a8f3988a8dacfaf0c216712c75
Jacob Sanford <jsanford@unb.ca>
2021-10-14 13:14:32 -0300

    Name .htaccess file properly

--- build/scripts/pre-init.d/71_set_config_sync_permissions.sh
+++ build/scripts/pre-init.d/71_set_config_sync_permissions.sh
@@ -3,5 +3,5 @@
 find ${DRUPAL_CONFIGURATION_DIR} \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} --
 
 # Add a .htaccess file that denies execution to satisfy Drupal.
-cp /security_htaccess/.htaccess.PreventExecution ${DRUPAL_CONFIGURATION_DIR}/.htaccess.PreventExecution
-chown root:root ${DRUPAL_CONFIGURATION_DIR}/.htaccess.PreventExecution
+cp /security_htaccess/.htaccess.PreventExecution ${DRUPAL_CONFIGURATION_DIR}/.htaccess
+chown root:root ${DRUPAL_CONFIGURATION_DIR}/.htaccess
