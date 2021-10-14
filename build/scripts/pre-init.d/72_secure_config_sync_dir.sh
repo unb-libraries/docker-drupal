66b52bc2bfb12b801c048dd4712f1f71ea5fc6c8
Jacob Sanford <jsanford@unb.ca>
2021-10-14 13:50:45 -0300

    Break out configuration secure script, add to cron

---
+++ build/scripts/pre-init.d/72_secure_config_sync_dir.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# Add a .htaccess file that denies execution to satisfy Drupal.
+cp /security_htaccess/.htaccess.PreventExecution ${DRUPAL_CONFIGURATION_DIR}/.htaccess
+chown root:root ${DRUPAL_CONFIGURATION_DIR}/.htaccess
