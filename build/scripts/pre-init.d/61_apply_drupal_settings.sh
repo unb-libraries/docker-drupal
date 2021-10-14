cea30a19378bd7cb804c473904806977c2243c0f
Jacob Sanford <jsanford@unb.ca>
2021-10-14 10:36:36 -0300

    Protect write access to settings.php

--- build/scripts/pre-init.d/61_apply_drupal_settings.sh
+++ build/scripts/pre-init.d/61_apply_drupal_settings.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 # Ensure local settings are applied.
 grep -q -F 'sites/all/settings/base.settings.php' "${DRUPAL_ROOT}/sites/default/settings.php" || echo "require DRUPAL_ROOT . '/sites/all/settings/base.settings.php';" >> "${DRUPAL_ROOT}/sites/default/settings.php"
+chown root:root "${DRUPAL_ROOT}/sites/default/settings.php"
+chmod -w "${DRUPAL_ROOT}/sites/default/settings.php"
