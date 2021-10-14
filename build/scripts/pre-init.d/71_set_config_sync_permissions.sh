f0b5ebad167a71cf677f8238dff427dc1b72f59e
Jacob Sanford <jsanford@unb.ca>
2021-10-14 13:50:45 -0300

    Break out configuration secure script, add to cron

--- build/scripts/pre-init.d/71_set_config_sync_permissions.sh
+++ build/scripts/pre-init.d/71_set_config_sync_permissions.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env sh
 # Allow the web daemon user to write to the config sync directory.
 find ${DRUPAL_CONFIGURATION_DIR} \! -user ${NGINX_RUN_USER} \! -group ${NGINX_RUN_GROUP} -print0 | xargs -r0 chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} --
-
-# Add a .htaccess file that denies execution to satisfy Drupal.
-cp /security_htaccess/.htaccess.PreventExecution ${DRUPAL_CONFIGURATION_DIR}/.htaccess
-chown root:root ${DRUPAL_CONFIGURATION_DIR}/.htaccess
