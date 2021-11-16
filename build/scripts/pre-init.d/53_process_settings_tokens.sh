d569a27321855dd487c5c098efdf0e152014cd38
Jacob Sanford <jsanford@unb.ca>
2021-11-16 12:28:51 -0400

    Tokenize Drupal configuration directory

--- build/scripts/pre-init.d/53_process_settings_tokens.sh
+++ build/scripts/pre-init.d/53_process_settings_tokens.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
 find ${DRUPAL_ROOT}/sites/all/settings -type f \( -name "*.php" -o -name "*.inc" \) -exec sed -i "s|DRUPAL_SITE_ID|$DRUPAL_SITE_ID|g" {} \;
+find ${DRUPAL_ROOT}/sites/all/settings -type f \( -name "*.php" -o -name "*.inc" \) -exec sed -i "s|DRUPAL_CONFIGURATION_DIR|$DRUPAL_CONFIGURATION_DIR|g" {} \;
