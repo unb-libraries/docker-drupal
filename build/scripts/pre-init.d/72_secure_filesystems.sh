361d0873ac957791a9b52006ce945409348f6a4d
Jacob Sanford <jsanford@unb.ca>
2021-12-02 10:09:54 -0400

    DOCKW-44 Allow Drupal images to deploy private filesystems simply

--- build/scripts/pre-init.d/72_secure_filesystems.sh
+++ build/scripts/pre-init.d/72_secure_filesystems.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env sh
-for FILESYSTEM in 'private://' 'public://'; do
-  FILESYSTEM_PATH=$($DRUSH eval "echo \Drupal::service('file_system')->realpath('$FILESYSTEM');")
-  if [ $FILESYSTEM_PATH ]; then
-    echo "Securing $FILESYSTEM [$FILESYSTEM_PATH]..."
-    $DRUSH eval 'use Drupal\Component\FileSecurity\FileSecurity; print FileSecurity::htaccessLines(FALSE)' > ${FILESYSTEM_PATH}/.htaccess
-    chown root:root ${FILESYSTEM_PATH}/.htaccess
-  fi
-done
+PUBLIC_FILESYSTEM_PATH=$($DRUSH eval "echo \Drupal::service('file_system')->realpath('public://');")
+if [ $PUBLIC_FILESYSTEM_PATH ]; then
+  echo "Securing public:// [$PUBLIC_FILESYSTEM_PATH]..."
+  $DRUSH eval 'use Drupal\Component\FileSecurity\FileSecurity; print FileSecurity::htaccessLines(FALSE)' > ${PUBLIC_FILESYSTEM_PATH}/.htaccess
+  chown root:root ${PUBLIC_FILESYSTEM_PATH}/.htaccess
+fi
+
+if [ ! -z "$DRUPAL_PRIVATE_FILE_PATH" ]; then
+  mkdir -p ${DRUPAL_PRIVATE_FILE_PATH} && chown ${NGINX_RUN_USER}:${NGINX_RUN_GROUP} ${DRUPAL_PRIVATE_FILE_PATH}
+  echo "Securing private:// [$DRUPAL_PRIVATE_FILE_PATH]..."
+  $DRUSH eval 'use Drupal\Component\FileSecurity\FileSecurity; print FileSecurity::htaccessLines(FALSE)' > ${DRUPAL_PRIVATE_FILE_PATH}/.htaccess
+  chown root:root ${DRUPAL_PRIVATE_FILE_PATH}/.htaccess
+fi
