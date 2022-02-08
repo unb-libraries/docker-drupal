070feada2e56e418660ec6f1c68525bf3c4b662e
Jacob Sanford <jsanford@unb.ca>
2022-02-08 08:54:31 -0400

    Add 3x config import issue link to config importer

--- build/scripts/pre-init.d/79_import_configuration.sh
+++ build/scripts/pre-init.d/79_import_configuration.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 # Configuration
 if [ -d "$DRUPAL_CONFIGURATION_DIR" ] && [ "$(ls $DRUPAL_CONFIGURATION_DIR)" ]; then
-  # What is this monstrosity? See https://github.com/drush-ops/drush/issues/2449.
+  # What is this monstrosity?
+  # Why do we import configuration 3 times?
+  # See: https://github.com/drush-ops/drush/issues/2449.
+  # Also: https://www.drupal.org/project/drupal/issues/3241439
   /scripts/configImport.sh || /scripts/configImport.sh
   /scripts/configImport.sh
 fi
