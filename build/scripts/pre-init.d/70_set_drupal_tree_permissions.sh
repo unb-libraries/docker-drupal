9dc24a078dab16e59f30e818df636603be66b5f6
Jacob Sanford <jsanford@unb.ca>
2022-02-08 13:50:03 -0400

    Remove 70_set_drupal_tree_permissions

--- build/scripts/pre-init.d/70_set_drupal_tree_permissions.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-# Prevent the web daemon user from modifying drupal tree files.
-# Disabled : I believe this is unnecessary.
-# find ${DRUPAL_ROOT} -not \( -path "${DRUPAL_ROOT}/sites/default/files" -prune \) -not -user root -not -group root -print0 | xargs -r0 chown root:root --
