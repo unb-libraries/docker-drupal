12bc5973dd0d573262e4768e4492c4c17637bbfe
Jacob Sanford <jsanford@unb.ca>
2022-02-08 13:55:40 -0400

    Correct logic in triage

--- build/scripts/pre-init.d/58_report_site_status.sh
+++ build/scripts/pre-init.d/58_report_site_status.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
-if [ ! -f /tmp/DRUPAL_DB_LIVE && ! -f /tmp/DRUPAL_FILES_LIVE ];
+if [ ! -f /tmp/DRUPAL_DB_LIVE ] && [ ! -f /tmp/DRUPAL_FILES_LIVE ];
 then
   echo "New installation detected..."
-elif [ -f /tmp/DRUPAL_DB_LIVE && -f /tmp/DRUPAL_FILES_LIVE ];
+elif [ -f /tmp/DRUPAL_DB_LIVE ] && [ -f /tmp/DRUPAL_FILES_LIVE ];
 then
   # Site Needs Upgrade
   echo "Existing installation detected..."
--- build/scripts/pre-init.d/78_update_database.sh
+++ build/scripts/pre-init.d/78_update_database.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # Apply any necessary database updates.
-if [ -f /tmp/DRUPAL_DB_LIVE ]  && [ -f /tmp/DRUPAL_FILES_LIVE ];
+if [ -f /tmp/DRUPAL_DB_LIVE ] && [ -f /tmp/DRUPAL_FILES_LIVE ];
 then
   echo "Applying database updates to existing instance..."
   $DRUSH updb
