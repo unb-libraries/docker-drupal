b8141bde325f956968d1a3eb4d7fb3f08807c0f6
Jacob Sanford <jsanford@unb.ca>
2022-04-29 14:51:06 -0300

    Revert "Disable newrelic distributed tracing"
    This reverts commit 25267d53f51c1e5de7e576a916561ec8715ab814.

--- build/scripts/pre-init.d/18_set_newrelic_env.sh
+++ build/scripts/pre-init.d/18_set_newrelic_env.sh
@@ -3,5 +3,4 @@ if [ -n "$DEPLOY_ENV" ] && [ -n "$NR_INSTALL_KEY" ] && [ ! "$DEPLOY_ENV" = "loca
   sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"$DEPLOY_ENV:$DRUPAL_SITE_URI\"|g" "$PHP_CONFD_DIR/newrelic.ini"
   sed -i "s|REPLACE_WITH_REAL_KEY|$NR_INSTALL_KEY|g" "$PHP_CONFD_DIR/newrelic.ini"
   echo "newrelic.labels = \"Environment:$DEPLOY_ENV;Node:$KUBERNETES_SERVICE_HOST;Framework:Drupal\"" >> "$PHP_CONFD_DIR/newrelic.ini"
-  echo "newrelic.distributed_tracing_enabled = false" >> "$PHP_CONFD_DIR/newrelic.ini"
 fi
