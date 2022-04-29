e7bee8dd16b1c1e0b3c934025bab2012a8cd4ccb
Jacob Sanford <jsanford@unb.ca>
2022-04-29 12:22:41 -0300

    Disable newrelic distributed tracing

--- build/scripts/pre-init.d/18_set_newrelic_env.sh
+++ build/scripts/pre-init.d/18_set_newrelic_env.sh
@@ -3,4 +3,5 @@ if [ -n "$DEPLOY_ENV" ] && [ -n "$NR_INSTALL_KEY" ] && [ ! "$DEPLOY_ENV" = "loca
   sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"${DEPLOY_ENV}:${DRUPAL_SITE_URI}\"|g" /etc/php7/conf.d/newrelic.ini
   sed -i "s|REPLACE_WITH_REAL_KEY|${NR_INSTALL_KEY}|g" /etc/php7/conf.d/newrelic.ini
   echo "newrelic.labels = \"Environment:${DEPLOY_ENV};Node:${KUBERNETES_SERVICE_HOST};Framework:Drupal\"" >> /etc/php7/conf.d/newrelic.ini
+  echo "newrelic.distributed_tracing_enabled = false" >> /etc/php7/conf.d/newrelic.ini
 fi
