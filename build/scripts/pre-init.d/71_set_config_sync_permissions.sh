8142ea297221a83f20831d36702f1954f7d108d9
Jacob Sanford <jsanford@unb.ca>
2022-05-24 09:54:34 -0300

    Ensure local users have write permissions to config dir

--- build/scripts/pre-init.d/71_set_config_sync_permissions.sh
+++ build/scripts/pre-init.d/71_set_config_sync_permissions.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 # Allow the web daemon user to write to the config sync directory.
-find "$DRUPAL_CONFIGURATION_DIR" \! -user "$NGINX_RUN_USER" \! -group "$NGINX_RUN_GROUP" -print0 | xargs -r0 chown "$NGINX_RUN_USER":"$NGINX_RUN_GROUP" --
+if [ "$DEPLOY_ENV" = "local" ] && [ -n "$LOCAL_USER_GROUP" ]; then
+  find "$DRUPAL_CONFIGURATION_DIR" \! -user "$NGINX_RUN_USER" \! -group "$LOCAL_USER_GROUP" -print0 | xargs -r0 chown "$NGINX_RUN_USER":"$LOCAL_USER_GROUP" --
+  chown "$NGINX_RUN_USER":"$LOCAL_USER_GROUP" "$DRUPAL_CONFIGURATION_DIR"
+else
+  find "$DRUPAL_CONFIGURATION_DIR" \! -user "$NGINX_RUN_USER" \! -group "$NGINX_RUN_GROUP" -print0 | xargs -r0 chown "$NGINX_RUN_USER":"$NGINX_RUN_GROUP" --
+  chown "$NGINX_RUN_USER":"$NGINX_RUN_GROUP" "$DRUPAL_CONFIGURATION_DIR"
+fi
