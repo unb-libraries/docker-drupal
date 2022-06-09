544d8a58f5a96012fbd7962f623ab7689609c60a
Jacob Sanford <jsanford@unb.ca>
2022-06-09 10:12:12 -0300

    Migrate from site-install to site:install

--- build/scripts/pre-init.d/60_install_drupal.sh
+++ build/scripts/pre-init.d/60_install_drupal.sh
@@ -12,5 +12,5 @@ then
 
   # Perform a drush site-install.
   cd "$DRUPAL_ROOT"
-  /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" "$DRUSH" site-install minimal --verbose --account-name="$DRUPAL_ADMIN_ACCOUNT_NAME" --account-pass="$DRUPAL_ADMIN_ACCOUNT_PASS" --db-url="mysqli://${DRUPAL_SITE_ID}_user:$DRUPAL_DB_PASSWORD@$MYSQL_HOSTNAME:$MYSQL_PORT/${DRUPAL_SITE_ID}_db"
+  /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`" "$DRUSH" site:install minimal --verbose --account-name="$DRUPAL_ADMIN_ACCOUNT_NAME" --account-pass="$DRUPAL_ADMIN_ACCOUNT_PASS" --db-url="mysqli://${DRUPAL_SITE_ID}_user:$DRUPAL_DB_PASSWORD@$MYSQL_HOSTNAME:$MYSQL_PORT/${DRUPAL_SITE_ID}_db"
 fi
