f713cb049bdd08dad83b2a72694e67190b4e2526
Jacob Sanford <jsanford@unb.ca>
2022-11-17 18:28:53 -0400

    Leverage list type of config:status

--- build/scripts/list_active_delta_stored_config_objects.sh
+++ build/scripts/list_active_delta_stored_config_objects.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
-DIFF_OUTPUT=$($DRUSH config:status 2>&1)
+DIFF_OUTPUT=$($DRUSH config:status --format=list 2>&1)
 if echo "$DIFF_OUTPUT" | grep -q "No differences"; then
-  echo "No differences between DB and sync directory"
   exit 0
 else
   echo "$DIFF_OUTPUT"
