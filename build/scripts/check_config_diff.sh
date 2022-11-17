af35bd88d31ef7d8e1416fc99234cb247e45e912
Jacob Sanford <jsanford@unb.ca>
2022-11-17 16:15:05 -0400

    Ensure output is routed appropriately

--- build/scripts/check_config_diff.sh
+++ build/scripts/check_config_diff.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-DIFF_OUTPUT=$($DRUSH config:status)
+DIFF_OUTPUT=$($DRUSH config:status 2>&1)
 if echo "$DIFF_OUTPUT" | grep -q "No differences"; then
   echo "No differences between DB and sync directory"
   exit 0
