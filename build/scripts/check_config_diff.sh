1f3490e2f9cd0139c94c8208d1e52d19c03613f0
Jacob Sanford <jsanford@unb.ca>
2022-11-17 15:34:38 -0400

    Add message if no config differences detected

--- build/scripts/check_config_diff.sh
+++ build/scripts/check_config_diff.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 DIFF_OUTPUT=$($DRUSH config:status)
 if echo "$DIFF_OUTPUT" | grep -q "No differences"; then
+  echo "No differences between DB and sync directory"
   exit 0
 else
   echo "$DIFF_OUTPUT"
