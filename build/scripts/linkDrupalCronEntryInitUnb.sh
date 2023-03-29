8e5f5ae55db94e879387d5a8120bd0ab128ac728
Jacob Sanford <jsanford@unb.ca>
2023-03-29 19:02:20 -0300

    Inject sanity into preinit naming

--- build/scripts/linkDrupalCronEntryInitUnb.sh
+++ build/scripts/linkDrupalCronEntryInitUnb.sh
@@ -10,4 +10,4 @@ ln -s /scripts/removeNewRelicIni.sh /scripts/pre-init.cron.d/21_remove_newrelic_
 ln -s /scripts/pre-init.d/56_create_private_filesystem.sh /scripts/pre-init.cron.d/
 
 # Ensure E-Mail in postfix queue gets sent before pod dies.
-ln -s /scripts/flushEmailsFromPostfix.sh /scripts/pre-init.cron.d/98_flush_postfix_queue.sh
+ln -s /scripts/flushEmailsFromPostfix.sh /scripts/pre-init.cron.d/94_flush_postfix_queue.sh
