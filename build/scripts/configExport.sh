6070f7b8beca75dd77025a82a5c0db2a2a0b67b7
Jacob Sanford <jsanford@unb.ca>
2023-05-01 16:42:03 -0300

    Dockworker now cleans up extraneous config

--- build/scripts/configExport.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-# Remove existing configuration.
-rm -rf "$DRUPAL_CONFIGURATION_DIR/*.yml"
-
-# Write out config.
-drush --root="$DRUPAL_ROOT" --uri=default --yes config-export --destination="$DRUPAL_CONFIGURATION_DIR"
-rm -rf "$DRUPAL_CONFIGURATION_DIR/*devel*"
