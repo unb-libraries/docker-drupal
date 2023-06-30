ea4c458544c9d3143592fe2eea6467ee0c2823ca
Jacob Sanford <jsanford@unb.ca>
2023-06-30 10:54:06 -0300

    Ensure nginx user has permission to write data

--- build/scripts/exportData.sh
+++ build/scripts/exportData.sh
@@ -7,6 +7,7 @@ BASE_PATH='/data-export'
 EXPORT_PATH="$BASE_PATH/$HASH"
 rm -rf "$EXPORT_PATH"
 mkdir -p "$EXPORT_PATH"
+chown $NGINX_RUN_USER:$NGINX_RUN_GROUP $EXPORT_PATH
 
 # Export Content
 /scripts/exportContent.sh "$EXPORT_PATH"
