123bb5e7d54274f93999d860038470eb23d4210d
Jacob Sanford <jsanford@unb.ca>
2023-07-01 09:25:48 -0300

    Allow data target for primary export script

--- build/scripts/exportData.sh
+++ build/scripts/exportData.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env sh
 set -e
 
-# Set up path location.
-HASH=$(echo $RANDOM | md5sum | head -c 20; echo)
-BASE_PATH='/data-export'
-EXPORT_PATH="$BASE_PATH/$HASH"
-rm -rf "$EXPORT_PATH"
-mkdir -p "$EXPORT_PATH"
-chown $NGINX_RUN_USER:$NGINX_RUN_GROUP $EXPORT_PATH
+if [ $# -ne 1 ];then
+  echo "No target location specified, generating random location."
+  HASH=$(echo $RANDOM | md5sum | head -c 20; echo)
+  BASE_PATH='/data-export'
+  EXPORT_PATH="$BASE_PATH/$HASH"
+  mkdir -p "$EXPORT_PATH"
+  chown $NGINX_RUN_USER:$NGINX_RUN_GROUP $EXPORT_PATH
+else
+  EXPORT_PATH="$1"
+fi
 
 # Export Content
 /scripts/exportContent.sh "$EXPORT_PATH"
