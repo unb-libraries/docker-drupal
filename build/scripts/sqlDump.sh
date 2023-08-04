c44ea6754be30a4ba47b7e4a189742ba80b22b03
Jacob Sanford <jsanford@unb.ca>
2023-08-04 14:35:13 -0300

    Investigate R-o filesystem

--- build/scripts/sqlDump.sh
+++ build/scripts/sqlDump.sh
@@ -5,5 +5,6 @@ if [ $# -ne 1 ];then
   echo "No target specified."
   exit 1
 fi
-
+ls -al /snapshot
+find /snapshot
 $DRUSH sql-dump --debug --extra-dump=--no-tablespaces --structure-tables-list="accesslog,batch,cache,cache_*,ctools_css_cache,ctools_object_cache,flood,search_*,history,queue,semaphore,sessions,watchdog" --result-file=$1
