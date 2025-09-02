8d416183875c2be21c0f95f33a38df76a5e318de
Jacob Sanford <jsanford@unb.ca>
2025-09-02 18:27:11 -0300

    Enable distributed tracing as we only enable for debug

--- build/scripts/pre-init.d/18_set_newrelic_env.sh
+++ build/scripts/pre-init.d/18_set_newrelic_env.sh
@@ -4,7 +4,7 @@ if [ "$ENABLE_NEWRELIC" = "true" ] && [ -n "$NR_INSTALL_KEY" ]; then
   sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"$DEPLOY_ENV:$DRUPAL_SITE_URI\"|g" "$PHP_CONFD_DIR/newrelic.ini"
   sed -i "s|REPLACE_WITH_REAL_KEY|$NR_INSTALL_KEY|g" "$PHP_CONFD_DIR/newrelic.ini"
   echo "newrelic.labels = \"Environment:$DEPLOY_ENV;Node:$KUBERNETES_SERVICE_HOST;Framework:Drupal\"" >> "$PHP_CONFD_DIR/newrelic.ini"
-  echo "newrelic.distributed_tracing_enabled = false" >> "$PHP_CONFD_DIR/newrelic.ini"
+  echo "newrelic.distributed_tracing_enabled = true" >> "$PHP_CONFD_DIR/newrelic.ini"
 else
   echo "New Relic not enabled or NR_INSTALL_KEY not set. Skipping configuration..."
   rm -f "$PHP_CONFD_DIR/newrelic.ini"
