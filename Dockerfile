e84581db90297447c101021574710a7de1820886
Jacob Sanford <jsanford@unb.ca>
2025-09-03 06:14:49 -0300

    Make explicit newrelic, tracing enabling

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM ghcr.io/unb-libraries/drupal:10.x-1.x
 
-ENV DRUPAL_REDIS_HOSTNAME drupal-redis-lib-unb-ca
+ENV DRUPAL_REDIS_HOSTNAME=drupal-redis-lib-unb-ca
+ENV ENABLE_NEWRELIC=false
+ENV ENABLE_NEWRELIC_TRACING=false
 
 COPY ./build /build
 RUN $RSYNC_MOVE /build/scripts/ /scripts/&& \
--- build/scripts/pre-init.d/18_set_newrelic_env.sh
+++ build/scripts/pre-init.d/18_set_newrelic_env.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 if [ "$ENABLE_NEWRELIC" = "true" ] && [ -n "$NR_INSTALL_KEY" ]; then
-  echo "Configuring New Relic..."
+  echo "Enabling New Relic..."
   sed -i "s|newrelic.appname = \"PHP Application\"|newrelic.appname = \"$DEPLOY_ENV:$DRUPAL_SITE_URI\"|g" "$PHP_CONFD_DIR/newrelic.ini"
   sed -i "s|REPLACE_WITH_REAL_KEY|$NR_INSTALL_KEY|g" "$PHP_CONFD_DIR/newrelic.ini"
   echo "newrelic.labels = \"Environment:$DEPLOY_ENV;Node:$KUBERNETES_SERVICE_HOST;Framework:Drupal\"" >> "$PHP_CONFD_DIR/newrelic.ini"
-  echo "newrelic.distributed_tracing_enabled = true" >> "$PHP_CONFD_DIR/newrelic.ini"
+  echo "newrelic.distributed_tracing_enabled = $ENABLE_NEWRELIC_TRACING" >> "$PHP_CONFD_DIR/newrelic.ini"
 else
   echo "New Relic not enabled or NR_INSTALL_KEY not set. Skipping configuration..."
   rm -f "$PHP_CONFD_DIR/newrelic.ini"
