cf091874914fe75e48094e21e53333c846c70fa2
Jacob Sanford <jsanford@unb.ca>
2025-10-09 11:16:01 -0300

    Increase clarity of cron

--- build/scripts/drupalSnapshotEntry.sh
+++ build/scripts/drupalSnapshotEntry.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
+
+# Cron is an efficient way to bootstrap Drupal without duplicating code.
+# Skipping the cron itself at the end.
 rm /scripts/pre-init.cron.d/94_drupal_cron.sh
 /scripts/drupalCronEntry.sh
 
