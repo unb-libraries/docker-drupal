51d944cff9ae11bfd30af80d48688dfa75c88cf9
Jacob Sanford <jsanford@unb.ca>
2025-11-18 13:19:49 -0400

    Ignore samlauth configuration if not enabled

--- build/scripts/pre-init.d/80_saml_entity_id.sh
+++ build/scripts/pre-init.d/80_saml_entity_id.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
-if [ "$DEPLOY_ENV" == "dev" ]; then
-  $DRUSH config-get samlauth.authentication  1> /dev/null 2> /dev/null && $DRUSH cset samlauth.authentication sp_entity_id "https://dev-$DRUPAL_SITE_URI"
+if ! grep -q samlauth "$DRUPAL_CONFIGURATION_DIR/core.extension.yml"; then
+  echo "SAMLAuth module not enabled, skipping dev SAML re-configuration."
+  exit 0
+fi
+
+if [ "$DEPLOY_ENV" = "dev" ]; then
+  $DRUSH config-get samlauth.authentication 1> /dev/null 2> /dev/null && $DRUSH cset samlauth.authentication sp_entity_id "https://dev-$DRUPAL_SITE_URI"
 fi
