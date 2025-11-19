01bab5455366e9650dbe7d8dabf20c6cc7492968
Jacob Sanford <jsanford@unb.ca>
2025-11-19 08:47:44 -0400

    Explicitly exit if triage is unexpected

--- build/scripts/pre-init.d/58_report_site_status.sh
+++ build/scripts/pre-init.d/58_report_site_status.sh
@@ -6,11 +6,10 @@ elif [ -f /tmp/DRUPAL_DB_LIVE ] && [ -f /tmp/DRUPAL_FILES_LIVE ];
 then
   # Site Needs Upgrade
   echo "Existing installation detected..."
-
-  # Ensure the database details are still valid.
-  sed -i "s|'host' => '.*',|'host' => '$DRUPAL_DB_HOSTNAME',|g" "$DRUPAL_ROOT/sites/default/settings.php"
-  sed -i "s|'port' => '[0-9]\{2,4\}',|'port' => '$DRUPAL_DB_PORT',|g" "$DRUPAL_ROOT/sites/default/settings.php"
 else
   # Inconsistency detected, do nothing to avoid data loss.
   echo "[Error] Something seems odd with the Database and Filesystem..."
+  echo "Database Live: $( [ -f /tmp/DRUPAL_DB_LIVE ] && echo 'Yes' || echo 'No' )"
+  echo "Files Live: $( [ -f /tmp/DRUPAL_FILES_LIVE ] && echo 'Yes' || echo 'No' )"
+  exit 1
 fi
