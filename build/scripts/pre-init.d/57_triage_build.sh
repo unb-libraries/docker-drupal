f19017285d2ad421234bd640a35903814314ec3c
Jacob Sanford <jsanford@unb.ca>
2025-11-19 07:23:52 -0400

    Update triage, leverage drush instead of MySQL show

--- build/scripts/pre-init.d/57_triage_build.sh
+++ build/scripts/pre-init.d/57_triage_build.sh
@@ -5,15 +5,17 @@
 rm -rf /tmp/DRUPAL_DB_LIVE
 rm -rf /tmp/DRUPAL_FILES_LIVE
 
-# Check if the database has tables named *node*. If so, this is likely a live DB.
-RESULT=$(mysqlshow -h "$MYSQL_HOSTNAME" -P "$MYSQL_PORT" --user="${DRUPAL_SITE_ID}_user" --password="$DRUPAL_DB_PASSWORD" --skip-ssl "${DRUPAL_SITE_ID}_db" | grep node)
-if [ ! -z "$RESULT" ]; then
-  touch /tmp/DRUPAL_DB_LIVE
-  echo "Triage : Found Drupal Database."
+# Determine if the site was previously built by checking for both .htaccess and settings.php in the public file dir.
+if [ -f "$DRUPAL_ROOT/sites/default/files/.htaccess" ] && [ -f "$DRUPAL_ROOT/sites/default/files/settings.php" ]; then
+  touch /tmp/DRUPAL_FILES_LIVE
+  echo "Triage : Found Drupal Filesystem and settings.php."
 fi
 
-# Determine if the site was previously built by checking the public file dir.
-if [ -f "$DRUPAL_ROOT/sites/default/files/.htaccess" ]; then
-  touch /tmp/DRUPAL_FILES_LIVE
-  echo "Triage : Found Drupal Filesystem."
+# Check if the database has tables named *node*. If so, this is likely a live DB.
+if [ -f /tmp/DRUPAL_FILES_LIVE ]; then
+  TABLE_COUNT=$($DRUSH sql-query "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name LIKE '%node%';" --skip-column-names)
+  if [ "$TABLE_COUNT" -gt 0 ]; then
+    touch /tmp/DRUPAL_DB_LIVE
+    echo "Triage : Found Drupal Database with $TABLE_COUNT node tables."
+  fi
 fi
