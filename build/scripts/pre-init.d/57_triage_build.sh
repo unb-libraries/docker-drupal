d94445ea560ba47ab8c7ed73a6d3fd353a94c75b
Jacob Sanford <jsanford@unb.ca>
2025-11-19 11:28:18 -0400

    Further explicit fail-stops

--- build/scripts/pre-init.d/57_triage_build.sh
+++ build/scripts/pre-init.d/57_triage_build.sh
@@ -5,7 +5,6 @@
 rm -rf /tmp/DRUPAL_DB_LIVE
 rm -rf /tmp/DRUPAL_FILES_LIVE
 
-
 # Test DB connection using environment variables, do NOT use Drush as we haven't bootstrapped Drupal.
 CONNECTION_TEST=$(mysql --host="$DRUPAL_DB_HOSTNAME" --port="$DRUPAL_DB_PORT" --user="$DRUPAL_DB_USER" --password="$DRUPAL_DB_PASSWORD" --database="$DRUPAL_DB_NAME" --execute="SELECT 1;" 2>&1)
 if echo "$CONNECTION_TEST" | grep -q "ERROR"; then
@@ -13,17 +12,22 @@ if echo "$CONNECTION_TEST" | grep -q "ERROR"; then
   echo "Refusing to proceed with build triage."
   exit 1
 else
-  TABLE_COUNT=$(mysql --host="$DRUPAL_DB_HOSTNAME" --port="$DRUPAL_DB_PORT" --user="$DRUPAL_DB_USER" --password="$DRUPAL_DB_PASSWORD" --database="$DRUPAL_DB_NAME" --skip-column-names --execute="SELECT COUNT(*) FROM information_schema.tables WHERE table_schma = DATABASE() AND table_name LIKE '%node%';" 2>/dev/null)
-  if [ "$TABLE_COUNT" -gt 0 ]; then
-    touch /tmp/DRUPAL_DB_LIVE
-    echo "Triage : Found Drupal Database with $TABLE_COUNT node tables."
+  TABLE_COUNT=$(mysql --host="$DRUPAL_DB_HOSTNAME" --port="$DRUPAL_DB_PORT" --user="$DRUPAL_DB_USER" --password="$DRUPAL_DB_PASSWORD" --database="$DRUPAL_DB_NAME" --skip-column-names --execute="SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name LIKE '%node%';" 2>/dev/null)
+  if echo "$TABLE_COUNT" | grep -Eq '^[0-9]+$'; then
+    if [ "$TABLE_COUNT" -gt 0 ]; then
+      touch /tmp/DRUPAL_DB_LIVE
+      echo "Triage : Found Drupal Database with $TABLE_COUNT node tables."
+    else
+      echo "Triage : Connected to database, but no node tables found."
+    fi
   else
-    echo "Triage : Connected to database, but no node tables found."
+    echo "Triage : Unexpected result from node table count: '$TABLE_COUNT'"
+    exit 1
   fi
 fi
 
 # Determine if the site was previously built by checking for settings.php and that a 'files' directory exists under $DRUPAL_PUBLIC_FILES_ROOT.
-DRUPAL_PUBLIC_FILES_ROOT="$DRUPAL_ROOT/sites/default/files"
+DRUPAL_PUBLIC_FILES_ROOT="$DRUPAL_ROOT/sites/default"
 if [ -f "$DRUPAL_PUBLIC_FILES_ROOT/settings.php" ] && [ -d "$DRUPAL_PUBLIC_FILES_ROOT/files" ]; then
   touch /tmp/DRUPAL_FILES_LIVE
   echo "Triage : Found Drupal Filesystem: settings.php and 'files' directory."
