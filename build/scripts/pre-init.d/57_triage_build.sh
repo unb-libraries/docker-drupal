2a56f3182c97e8b5491a83edb275409cd4154b07
Jacob Sanford <jsanford@unb.ca>
2025-11-19 09:31:04 -0400

    DB check should NOT depend on file check

--- build/scripts/pre-init.d/57_triage_build.sh
+++ build/scripts/pre-init.d/57_triage_build.sh
@@ -5,17 +5,26 @@
 rm -rf /tmp/DRUPAL_DB_LIVE
 rm -rf /tmp/DRUPAL_FILES_LIVE
 
+# Check if we can connect to the database, and a node table exists.
+# DO not use drush as we are unsure of the filesystem state at this point.
+# Use the DRUPAL_DB_* environment variables set in the container.
+DB_CHECK=$( \
+  mysql \
+    --host="$DRUPAL_DB_HOSTNAME" \
+    --port="$DRUPAL_DB_PORT" \
+    --user="$DRUPAL_DB_USER" \
+    --password="$DRUPAL_DB_PASSWORD" \
+    --database="$DRUPAL_DB_NAME" \
+    --execute="SHOW TABLES LIKE 'node';" 2>/dev/null \
+)
+if [ "$DB_CHECK" = "Tables_in_${DRUPAL_DB_NAME} (node)" ] || [ "$DB_CHECK" = "node" ]; then
+  touch /tmp/DRUPAL_DB_LIVE
+  echo "Triage : Found Drupal Database with node table."
+fi
+
 # Determine if the site was previously built by checking for both .htaccess and settings.php in the public file dir.
 if [ -f "$DRUPAL_ROOT/sites/default/files/.htaccess" ] && [ -f "$DRUPAL_ROOT/sites/default/files/settings.php" ]; then
   touch /tmp/DRUPAL_FILES_LIVE
   echo "Triage : Found Drupal Filesystem and settings.php."
 fi
 
-# Check if the database has tables named *node*. If so, this is likely a live DB.
-if [ -f /tmp/DRUPAL_FILES_LIVE ]; then
-  TABLE_COUNT=$($DRUSH sql-query "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = DATABASE() AND table_name LIKE '%node%';" --skip-column-names)
-  if [ "$TABLE_COUNT" -gt 0 ]; then
-    touch /tmp/DRUPAL_DB_LIVE
-    echo "Triage : Found Drupal Database with $TABLE_COUNT node tables."
-  fi
-fi
