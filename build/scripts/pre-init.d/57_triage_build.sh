f28f4ae2e5c3028a18737bb8ff03d063b48cf3bf
Jacob Sanford <jsanford@unb.ca>
2026-01-15 11:23:08 -0400

    Check for .htaccess in Drupal filesystem validation

--- build/scripts/pre-init.d/57_triage_build.sh
+++ build/scripts/pre-init.d/57_triage_build.sh
@@ -25,9 +25,9 @@ fi
 
 # Determine if the site was previously built by checking for settings.php and that a 'files' directory exists under $DRUPAL_PUBLIC_FILES_ROOT.
 DRUPAL_PUBLIC_FILES_ROOT="$DRUPAL_ROOT/sites/default"
-if [ -f "$DRUPAL_PUBLIC_FILES_ROOT/settings.php" ] && [ -d "$DRUPAL_PUBLIC_FILES_ROOT/files" ]; then
+if [ -f "$DRUPAL_PUBLIC_FILES_ROOT/.htaccess" ] && [ -d "$DRUPAL_PUBLIC_FILES_ROOT/files" ] && [ -f "$DRUPAL_PUBLIC_FILES_ROOT/files/.htaccess" ]; then
   touch /tmp/DRUPAL_FILES_LIVE
-  echo "Triage : Found Drupal Filesystem: settings.php and 'files' directory."
+  echo "Triage : Found Drupal Filesystem: .htaccess and 'files/.htaccess' directory."
 else
   echo "Triage : settings.php or 'files' directory not found."
 fi
