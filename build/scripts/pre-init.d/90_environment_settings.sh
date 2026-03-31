7c0ca9316e22d696eb7579d20b7a6139aeedaa6d
Jacob Sanford <jsanford@unb.ca>
2026-03-31 06:45:35 -0300

    Audit site for enabled non-prod-cool modules

--- build/scripts/pre-init.d/90_environment_settings.sh
+++ build/scripts/pre-init.d/90_environment_settings.sh
@@ -2,7 +2,7 @@
 if [ "$DEPLOY_ENV" = "prod" ]; then
   FORBID_MODULES="devel field_ui views_ui dblog"
   for MODULE in $FORBID_MODULES; do
-    if ${DRUSH} pm-list --status=enabled --type=module | grep -q "^$MODULE "; then
+    if ${DRUSH} pm:list --status=enabled --type=module --format=list | grep -q "^$MODULE$"; then
       echo "Error: Module '$MODULE' is enabled in production environment. Please disable it before deploying."
       exit 1
     fi
