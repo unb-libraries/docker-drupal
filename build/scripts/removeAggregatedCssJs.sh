25784f07c4ef7b5566cc0ac00bcc2f52657e0a10
Jacob Sanford <jsanford@unb.ca>
2026-04-27 10:13:25 -0300

    Fix bugs in import

--- build/scripts/removeAggregatedCssJs.sh
+++ build/scripts/removeAggregatedCssJs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-rm -rf "$DRUPAL_ROOT/sites/default/files/css/*.css"
-rm -rf "$DRUPAL_ROOT/sites/default/files/js/*.js"
+rm -f "$DRUPAL_ROOT/sites/default/files/css/"*.css 2>/dev/null || true
+rm -f "$DRUPAL_ROOT/sites/default/files/js/"*.js 2>/dev/null || true
