79bf1d9133a733d9c22af4c9e7d50de9226a7a4b
Jacob Sanford <jsanford@unb.ca>
2026-06-04 21:31:47 -0300

    Update glob expansion

--- build/scripts/configExport.sh
+++ build/scripts/configExport.sh
@@ -8,5 +8,5 @@ else
   chown "$NGINX_RUN_USER":"$NGINX_RUN_GROUP" "$DRUPAL_CONFIGURATION_DIR"
 fi
 
-rm -rf "$DRUPAL_CONFIGURATION_DIR/*.yml"
+rm -rf "$DRUPAL_CONFIGURATION_DIR"/*.yml
 $DRUSH config-export --destination="$DRUPAL_CONFIGURATION_DIR"
